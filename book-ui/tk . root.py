97106e59ae2b0a3d2d6efe17448bb811a53ff70c
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-03-27 15:09:46 +0800

    Update tk . root.py

--- book-ui/tk . root.py
+++ book-ui/tk . root.py
@@ -1,4 +1,4 @@
-	root = tk.Tk()
-	root.resizable(False, False)    ;# Fixed UI size
-	root.title("AUTO UI Control") 	;# UI Title text
-	root.geometry( programSize )		;# Define UI size
+root = tk.Tk()
+root.resizable(False, False)    ;# Fixed UI size
+root.title("AUTO UI Control") 	;# UI Title text
+root.geometry( programSize )	;# Define UI size
