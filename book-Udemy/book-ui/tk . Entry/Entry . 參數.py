7351ae36c19245dcbb5fb7a75f2e310e0b75b4e0
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-01 15:49:37 +0800

    Create Entry . 參數.py

---
+++ book-Udemy/book-ui/tk . Entry/Entry . 參數.py
@@ -0,0 +1,7 @@
+tk.Entry(f2_fflow1, textvariable=var_flow1_x, fg="#FFAAD5", bg="#7AFEC6", font=("Algerian","16","bold"), width=5)
+
+textvariable=var_flow1_x
+fg="#FFAAD5"
+bg="#7AFEC6"
+font=("Algerian","16","bold")
+width=25
