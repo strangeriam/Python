f510a45ea0bfb5b395a234b2ec5575ba703acc11
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-01 16:26:00 +0800

    Create Entry . 參數 2.py

---
+++ book-Udemy/book-ui/tk . Entry/Entry . 參數 2.py
@@ -0,0 +1,14 @@
+Entry 元件常用參數如下表 : 
+
+
+ Entry 元件常用屬性	 說明
+ textvariable	 動態綁定文字欄位內容的類別變數物件名稱
+ show	 輸入資料時顯示的固定字元, 例如用於密碼欄位
+ width	 寬度 (單位=字元數)
+ height	 高度 (單位=字元數)
+ bg/background	 背景色, 可用標準顏色名稱字串例如 'blue' 或 "#0000ff"
+ fg/foreground	 前景色 (文字顏色), 可用標準顏色名稱字串例如 'blue' 或 "#0000ff"
+ font	 設定字型與尺寸 (px), 粗體 (bold) 或斜體 (italic), 底線或刪除線等 
+ padx	 元件與容器的水平間距 (px)
+ pady	 元件與容器的垂直間距 (px)
+ state	 設定按鈕狀態=tk.NORMAL (預設可用)/tk.DISABLED (不可用)
