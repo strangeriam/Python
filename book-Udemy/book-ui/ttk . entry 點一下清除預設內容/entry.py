4e6ae5efa66474792057e08e5403d3d02b18fe34
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-01 13:51:19 +0800

    Update entry.py

--- book-Udemy/book-ui/ttk . entry 點一下清除預設內容/entry.py
+++ book-Udemy/book-ui/ttk . entry 點一下清除預設內容/entry.py
@@ -1,3 +1,8 @@
+
+# UI 呈現說明:
+# 兩個 Entry 元件都呼叫 bind() 方法監聽滑鼠左鍵的按一下鈕 (Button-1) 事件, 並綁定事件處理函式.
+# 在事件處理函式中, 呼叫 Entry 物件的 delete(0, tk.END) 方法即可刪除 Entry 內預設值或先前輸入的內容.
+
 import tkinter as tk
 from tkinter import ttk
 
