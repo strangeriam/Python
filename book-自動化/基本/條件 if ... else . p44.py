a75cf1eae59fb1ae162a2ca240eb133afa2496c4
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-01 08:50:46 +0800

    Update 條件 if ... else . p44.py

--- book-自動化/基本/條件 if ... else . p44.py
+++ book-自動化/基本/條件 if ... else . p44.py
@@ -1,12 +1,32 @@
+# 條件 A
+if 'A':
+    ...
+elif 'B':
+    ...
+else:
+    ...
+
+# 條件 B
+'=='
+'<'
+'>'
+
+# Example
 name = 'Lu'
 
 # '=='
 if name == 'Lu':
     print('Hello Lu')
-
     if password == 'swordfish':
         print('Access granted.')
     else:
         print('Wrong password.')
+elif age < 12:
+    print('You are not Lu, Kiddo.') ;# kiddo 小子
+
+if age > 2000:
+    print('Unlike you, Lu is not an undead, immprtal vampire.')
+elif age > 100:
+    print('You are not Lu, grannie.')
 
 
