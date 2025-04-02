ee15e7e680cbb75736bca001898002c7cd47f514
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-02 14:34:26 +0800

    Update and rename 02 . 取 RGBA 值 . getcolor() . p519 to 02 . 取 RGBA 值 . getcolor() . p519.py

---
+++ book-自動化/Chapter 19 . 處理影像圖片 Pillow/02 . 取 RGBA 值 . getcolor() . p519.py
@@ -0,0 +1,21 @@
+from PIL import ImageColor
+
+>>> ImageColor.getcolor('red', 'RGBA')
+
+>>> from PIL import ImageColor
+>>> ImageColor.getcolor('red', 'RGBA')
+(255, 0, 0, 255)
+>>>
+>>> ImageColor.getcolor('RED', 'RGBA')
+(255, 0, 0, 255)
+>>>
+>>> ImageColor.getcolor('Black', 'RGBA')
+(0, 0, 0, 255)
+>>>
+>>> ImageColor.getcolor('chocolate', 'RGBA')
+(210, 105, 30, 255)
+>>>
+>>> ImageColor.getcolor('CornflowerBlue', 'RGBA')
+(100, 149, 237, 255)
+>>>
+>>>
