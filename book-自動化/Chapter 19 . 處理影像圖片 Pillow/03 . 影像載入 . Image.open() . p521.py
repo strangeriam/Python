6b34ebdaa88ea1eb9bd6960e13cd558bf456b445
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-02 15:09:05 +0800

    Update 03 . 影像載入 . Image.open() . p521.py

--- book-自動化/Chapter 19 . 處理影像圖片 Pillow/03 . 影像載入 . Image.open() . p521.py
+++ book-自動化/Chapter 19 . 處理影像圖片 Pillow/03 . 影像載入 . Image.open() . p521.py
@@ -2,5 +2,29 @@ import os
 # 切換圖檔所在工作目錄.
 os.chdir('D:\\BeeStation\\02_python\\250402_mouseMove4\\')
 
-from PIL import Image
-catIm = Image.open('sample.png')
+# 圖檔資訊:
+# filename: sample.png
+# size 642 x 232
+
+>>> from PIL import Image
+>>> catIm = Image.open('sample.png')
+
+>>> catIm.size
+(642, 232)
+
+>>> width, height = catIm.size
+>>> width
+642
+
+>>> height
+232
+
+>>> catIm.filename
+'sample.png'
+
+>>> catIm.format
+'PNG'
+
+>>> catIm.format_description
+'Portable network graphics'
+
