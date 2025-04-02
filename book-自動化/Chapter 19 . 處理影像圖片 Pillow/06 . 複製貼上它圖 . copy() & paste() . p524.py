3402d6567d9ee9101412295fb3a3ff58a5588e1c
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-02 16:33:01 +0800

    Update 06 . 複製貼上它圖 . copy() & paste() . p524.py

--- book-自動化/Chapter 19 . 處理影像圖片 Pillow/06 . 複製貼上它圖 . copy() & paste() . p524.py
+++ book-自動化/Chapter 19 . 處理影像圖片 Pillow/06 . 複製貼上它圖 . copy() & paste() . p524.py
@@ -5,3 +5,10 @@ from PIL import Image
 catIm = Image.open('zophie.png')
 catCopyIm = catIm.copy()
 
+>>> faceIm = catIm.crop((335, 345, 565, 560))
+>>> faceIm.size
+(230, 215)
+
+catCopyIm.paste(faceIm, (0, 0))
+catCopyIm.paste(faceIm, (400, 500))
+catCopyIm.save('pasted.png')
