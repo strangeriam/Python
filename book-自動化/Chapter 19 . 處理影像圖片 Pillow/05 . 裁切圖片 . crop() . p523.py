b3981ddc81ea6f753a28d2b1fc9cb35159fdb352
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 09:24:43 +0800

    Update 05 . 裁切圖片 . crop() . p523.py

--- book-自動化/Chapter 19 . 處理影像圖片 Pillow/05 . 裁切圖片 . crop() . p523.py
+++ book-自動化/Chapter 19 . 處理影像圖片 Pillow/05 . 裁切圖片 . crop() . p523.py
@@ -6,5 +6,5 @@ os.chdir('D:\\BeeStation\\02_python\\250402_mouseMove4\\')
 from PIL import Image
 catIM = Image.open('zophie.png')
 
-croppedIm = catIM.crop((335, 345, 565, 560))
+croppedIm = catIM.crop((335, 345, 565, 560)) # crop 修剪
 croppedIm.save('cropped.png')
