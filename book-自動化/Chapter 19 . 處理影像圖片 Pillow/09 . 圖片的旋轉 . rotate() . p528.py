307d6029a5a41bafb3dd9e73f884055e2ee70e45
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 10:14:56 +0800

    Update 09 . 圖片的旋轉 . rotate() . p528.py

--- book-自動化/Chapter 19 . 處理影像圖片 Pillow/09 . 圖片的旋轉 . rotate() . p528.py
+++ book-自動化/Chapter 19 . 處理影像圖片 Pillow/09 . 圖片的旋轉 . rotate() . p528.py
@@ -14,3 +14,8 @@ catIm = Image.open('zophie.png')
 catIm.rotate(90).save('rotated90.png') # rotate 旋轉
 catIm.rotate(180).save('rotated180.png')
 catIm.rotate(270).save('rotated270.png')
+
+
+# expand=True 修正旋轉後的版面.
+catIm.retate(6).save('rotated6.png')
+catIm.rotate(6, expand=True).save('rotate6_expanded.png')
