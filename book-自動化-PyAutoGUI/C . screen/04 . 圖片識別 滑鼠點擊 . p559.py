861dbbee720fb17d05326969b456f92153f17dce
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 14:19:51 +0800

    Update 04 . 圖片識別 滑鼠點擊 . p559.py

--- book-自動化-PyAutoGUI/C . screen/04 . 圖片識別 滑鼠點擊 . p559.py
+++ book-自動化-PyAutoGUI/C . screen/04 . 圖片識別 滑鼠點擊 . p559.py
@@ -36,3 +36,12 @@ pyautogui.click((1706, 256, 91, 106))
 # Way 2
 pyautogui.click('submit.png')
 # OUTPUT: click normal ..  (2025, 4, 7, 14, 12, 54)
+
+# 如果在螢幕上找不到符合的圖片時, locateOnScreen() 會引發例外異常.
+# 所以最好要在 try 陳述句中呼叫.
+
+try:
+  location = pyautogui.locateOnScreen('submit.png')
+except:
+  print('Image could not be found')
+
