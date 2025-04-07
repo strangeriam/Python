3621d82888f456b6bae96f17ae5f774d9c48e29f
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 09:24:06 +0800

    Update 07 . 用 Zophie 的臉貼滿整個圖 . p526.py

--- book-自動化/Chapter 19 . 處理影像圖片 Pillow/07 . 用 Zophie 的臉貼滿整個圖 . p526.py
+++ book-自動化/Chapter 19 . 處理影像圖片 Pillow/07 . 用 Zophie 的臉貼滿整個圖 . p526.py
@@ -1,5 +1,19 @@
+import os
+os.chdir('D:\\BeeStation\\02_python\\250402_mouseMove4\\')
 
+from PIL import Image
+catIm = Image.open('zophie.png')
+catCopyIm = catIm.copy()
 
+faceIm = catIm.crop((335, 345, 565, 560)) # crop 修剪
+faceIm.size
+# OUTPUT: (230, 215)
+
+catCopyIm.paste(faceIm, (0, 0))
+catCopyIm.paste(faceIm, (400, 500))
+catCopyIm.save('pasted.png')
+
+# Page 526
 catImWidth, catImHeight = catIm.size
 faceImWidth, faceImHeight = faceIm.size
 catCopyTwo = catIm.copy()
