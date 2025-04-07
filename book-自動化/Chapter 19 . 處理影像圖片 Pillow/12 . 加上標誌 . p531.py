a74186d1261403160577cba71a38754ff3d77d56
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 10:56:21 +0800

    Update 12 . 加上標誌 . p531.py

--- book-自動化/Chapter 19 . 處理影像圖片 Pillow/12 . 加上標誌 . p531.py
+++ book-自動化/Chapter 19 . 處理影像圖片 Pillow/12 . 加上標誌 . p531.py
@@ -9,3 +9,13 @@ LOGO_FILENAME = 'catlogo.png'
 logoIm = Image.open(LOGO_FILENAME)
 logoWidth, logoHeight = logoIm.size
 
+os.makedirs('withLogo', exist_ok=True)
+
+for filename in os.listdir('.'):
+  if not (filename.endswith('.png') or filename.endswith('.jpg')) \
+  or filename == LOGO_FILENAME:
+    continue
+  im = Image.open(filename)
+  width, height = im.size
+
+Lu- 250407: 未完成, 有空再學...
