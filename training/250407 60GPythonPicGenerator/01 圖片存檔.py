adac13f79991a476e8205f067d76c64081fae021
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 16:10:06 +0800

    Create 01 圖片存檔.py

---
+++ training/250407 60GPythonPicGenerator/01 圖片存檔.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import os
+Path.cwd()
+
+# OUTPUT:
+WindowsPath('C:/Users/Rlulu/AppData/Local/Programs/Python/Python311')
+
+import os
+os.chdir('D:\\BeeStation\\02_python\\250402_mouseMove4\\')
