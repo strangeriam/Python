f4b65854db9ff8cfb198398dbd1b85785fcf9a04
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 15:36:30 +0800

    Update 10 . 操控視窗 . p563.py

--- book-自動化-PyAutoGUI/C . screen/10 . 操控視窗 . p563.py
+++ book-自動化-PyAutoGUI/C . screen/10 . 操控視窗 . p563.py
@@ -1,12 +1,49 @@
 import pyautogui
 
 >>> fw = pyautogui.getActiveWindow()
->>> fw.width # 取得視窗寬度.
+
+# 取得視窗寬度.
+>>> fw.width
 878
 
->>> fw.topleft # 取得視窗位置.
+# 取得視窗位置.
+>>> fw.topleft 
 Point(x=-2532, y=-95)
->>>
 
->>> fw.width = 500 # 調整視窗寬度.
->>> fw.topleft = (800, 400) # 移動視窗到指定座標.
+# 調整視窗寬度.
+>>> fw.width = 500
+
+# 移動視窗到指定座標.
+>>> fw.topleft = (800, 400)
+
+# 視窗是否最大化
+>>> fw.isMaximized
+False
+
+# 視窗是否最小化
+>>> fw.isMinimized
+False
+
+# 視窗是否正在使用
+>>> fw.isActive
+True
+
+# 最大化視窗
+>>> fw.maximize()
+>>> fw.isMaximized
+True
+
+# 重作先前的狀態, Lu-250407: 沒感覺.
+>>> fw.restore()
+
+# 最小化視窗
+>>> fw.minimize()
+
+import time
+time.sleep(5); fw.activate()
+
+# 關閉視窗
+fw.close()
+
+
+
