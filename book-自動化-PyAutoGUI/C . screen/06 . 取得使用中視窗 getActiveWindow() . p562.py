c5f653d0acc7e3e863811b5cc3604bad7880b4cd
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 14:39:04 +0800

    Update 06 . 取得使用中視窗 getActiveWindow() . p562.py

--- book-自動化-PyAutoGUI/C . screen/06 . 取得使用中視窗 getActiveWindow() . p562.py
+++ book-自動化-PyAutoGUI/C . screen/06 . 取得使用中視窗 getActiveWindow() . p562.py
@@ -1,2 +1,13 @@
 # 程式目的: 較可靠方法.
+# 取得 Window 物件後, 就可以截取所有屬性.
 
+left, right, top, bottom
+topleft, topright, bottomleft, bottomright
+midleft, midright, midleft, midright
+width, height
+size
+area
+center
+centerx, centery
+box 視窗方塊 (left, top, width, height)
+title 視窗頂端標題文字
