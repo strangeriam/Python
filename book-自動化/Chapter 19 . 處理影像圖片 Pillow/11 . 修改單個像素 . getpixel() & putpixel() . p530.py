ccb4391e501bd516561dddf1bb96f2a3bbe28b01
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 10:38:56 +0800

    Update 11 . 修改單個像素 . getpixel() & putpixel() . p530.py

--- book-自動化/Chapter 19 . 處理影像圖片 Pillow/11 . 修改單個像素 . getpixel() & putpixel() . p530.py
+++ book-自動化/Chapter 19 . 處理影像圖片 Pillow/11 . 修改單個像素 . getpixel() & putpixel() . p530.py
@@ -1,15 +1,17 @@
+# 程式流程
+
 import os
 os.chdir('D:\\BeeStation\\02_python\\250402_mouseMove4\\')
 
 from PIL import Image
-im = Image.new('RGBA', (100, 100))
-im.getpixel((0, 0))
+im = Image.new('RGBA', (100, 100)) # --> 先取得一個 100 x 100 的透明正方形.
+im.getpixel((0, 0)) # --> 對一些座標呼叫 getpixel() 會返回 0, 0, 0, 0
 
 # OUTPUT: (0, 0, 0, 0)
 
-for x in range(100):
+for x in range(100): # --> 用 putpixel() 設定每個像素的色彩.
   for y in range(50):
-    im.putpixel((x, y), (210, 210, 210))
+    im.putpixel((x, y), (210, 210, 210)) # --> 灰色.
 
 from PIL import ImageColor
 for x in range(100):
