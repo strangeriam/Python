1775bef164daa682998c0aed7cedbf28f9ed579e
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 07:56:17 +0800

    Update 01 . time() . p452.py

--- book-自動化/Chapter 17 . 保持時間 工作排程 和啟動/01 . time() . p452.py
+++ book-自動化/Chapter 17 . 保持時間 工作排程 和啟動/01 . time() . p452.py
@@ -1,6 +1,6 @@
-Unix 紀元 (Unix epoch)
-從 1970 年 1 月 1 日 0 點, 世界標準時間 (UTC).
-time.time() 函式會返回從那一刻起的秒數.
+# Unix 紀元 (Unix epoch)
+# 從 1970 年 1 月 1 日 0 點, 世界標準時間 (UTC).
+# time.time() 函式會返回從那一刻起的秒數.
 
 >>> import time
 >>> time.time()
