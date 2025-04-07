cef4b4017d19aba9ce428e9fb690b77d5844a786
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 15:08:32 +0800

    Update 06 . 取得使用中視窗 getActiveWindow() . p562.py

--- book-自動化-PyAutoGUI/C . screen/06 . 取得使用中視窗 getActiveWindow() . p562.py
+++ book-自動化-PyAutoGUI/C . screen/06 . 取得使用中視窗 getActiveWindow() . p562.py
@@ -11,3 +11,29 @@ center
 centerx, centery
 box 視窗方塊 (left, top, width, height)
 title 視窗頂端標題文字
+
+
+import pyautogui
+>>> fw = pyautogui.getActiveWindow()
+>>> fw
+Win32Window(hWnd=531288)
+>>>
+
+>>> str(fw)
+'<Win32Window left="-2532", top="-95", width="673", height="519", title="Python 3.11 (64-bit)">'
+>>>
+>>> fw.title
+'Python 3.11 (64-bit)'
+>>>
+>>> fw.size
+Size(width=878, height=539)
+>>>
+>>> fw.left, fw.top, fw.right, fw.bottom
+(-2532, -95, -1654, 444)
+>>>
+>>> fw.area
+473242
+>>>
+>>> pyautogui.click(fw.left + 10, fw.top + 20)
+>>>
+
