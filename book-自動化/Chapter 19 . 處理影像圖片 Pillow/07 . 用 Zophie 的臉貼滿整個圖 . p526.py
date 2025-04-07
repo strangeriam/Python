81ff7041033b532ef587eb5faef31f6070fa10a3
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-07 09:28:07 +0800

    Update 07 . 用 Zophie 的臉貼滿整個圖 . p526.py

--- book-自動化/Chapter 19 . 處理影像圖片 Pillow/07 . 用 Zophie 的臉貼滿整個圖 . p526.py
+++ book-自動化/Chapter 19 . 處理影像圖片 Pillow/07 . 用 Zophie 的臉貼滿整個圖 . p526.py
@@ -22,3 +22,30 @@ for left in range(0, catImWidth, faceImWidth):
   for top in range(0, catImHeight, faceImHeight):
     print(left, top)
     catCopyTwo.paste(faceIm, (left, top))
+
+# OUTPUT:
+0 0
+0 215
+0 430
+0 645
+0 860
+0 1075
+230 0
+230 215
+230 430
+230 645
+230 860
+230 1075
+460 0
+460 215
+460 430
+460 645
+460 860
+460 1075
+690 0
+690 215
+690 430
+690 645
+690 860
+690 1075
+[Finished in 284ms]
