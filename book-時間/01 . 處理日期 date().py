289afe7d18bf025c9c76c70233ab932248e0f93d
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 09:14:29 +0800

    Update 01 . 處理日期 date().py

--- book-時間/01 . 處理日期 date().py
+++ book-時間/01 . 處理日期 date().py
@@ -1,17 +1,5 @@
 # Reference link: https://steam.oxxostudio.tw/category/python/library/datetime.html#a1
 
-# datetime 有下列幾種處理日期和時間的方法
-#
-# date	處理日期
-# time	處理時間
-# datetime	處理 date 和 time 混合的物件
-# timedelta	處理時間差
-# timezone	處理時區資訊
-
-import datetime
-from datetime import date
-
-
 import datetime
 d = datetime.date(2025,4,8)
 
