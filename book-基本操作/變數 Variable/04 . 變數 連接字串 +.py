7bb22f8d75e8b4a63f4022e3ff3ea10f9d122b0c
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 10:53:35 +0800

    Update 04 . 變數 連接字串 +.py

--- book-基本操作/變數 Variable/04 . 變數 連接字串 +.py
+++ book-基本操作/變數 Variable/04 . 變數 連接字串 +.py
@@ -2,11 +2,9 @@ import datetime
 nowDate = datetime.datetime.now().strftime('%Y%m%d')
 nowTime = datetime.datetime.now().strftime('%H%M%S')
 
-print('Date: ', nowDate)
-print('Time: ', nowTime)
+print('Date: ', nowDate) # Date:  20250408
+print('Time: ', nowTime) # Time:  104957
 
-filename = 'report_' + nowTime
+filename = 'report_' + nowTime + '.txt'
 
-print(filename)
-
-# OUTPUT: report_104957
+print(filename) # report_104957.txt
