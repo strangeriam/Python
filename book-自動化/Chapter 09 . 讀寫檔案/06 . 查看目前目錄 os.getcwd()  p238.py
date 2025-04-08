f620d69094df29b6bc6018a0bd7252cb64bd81dc
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 15:54:30 +0800

    Update and rename os.getcwd . 查看目前目錄 .py to 06 . 查看目前目錄 os.getcwd()  p238.py

---
+++ book-自動化/Chapter 09 . 讀寫檔案/06 . 查看目前目錄 os.getcwd()  p238.py
@@ -0,0 +1,4 @@
+# 此為預設功能, 不須再呼叫其他 lib.
+
+import os
+os.getcwd() # 'C:\\Users\\Rlulu\\AppData\\Local\\Programs\\Python\\Python311'
--- book-自動化/Chapter 09 . 讀寫檔案/os.getcwd . 查看目前目錄 .py
+++
@@ -1,8 +0,0 @@
-此為預設功能, 不須再呼叫其他 lib.
-
->>> import os
->>> os.getcwd()
-'C:\\Users\\Rlulu\\AppData\\Local\\Programs\\Python\\Python311'
->>>
-
-
