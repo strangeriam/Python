dd3f42da35e3ebc7f1bcf8074ec0defca2b80c0d
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 16:23:52 +0800

    Update 11 . 相對路徑 . p241.py

--- book-自動化/Chapter 09 . 讀寫檔案/11 . 相對路徑 . p241.py
+++ book-自動化/Chapter 09 . 讀寫檔案/11 . 相對路徑 . p241.py
@@ -1,6 +1,14 @@
 
+# 切換工作目錄
+import os
+os.chdir('D:/BeeStation/02_python/coding')
+
 from pathlib import Path
+Path('my/relative/path') # WindowsPath('my/relative/path')
+Path.cwd() / Path('my/relative/path') # WindowsPath('D:/BeeStation/02_python/coding/my/relative/path')
 
-Path('my/relative/path')
-
-Path.cwd() / Path('my/relative/path')
+# 如果相對路徑 在目前工作目錄之外
+# 以下是在 家目錄, 不在目前工作目錄.
+from pathlib import Path
+Path('my/relative/path') # WindowsPath('my/relative/path')
+Path.home() / Path('my/relative/path') # WindowsPath('C:/Users/Rlulu/my/relative/path')
