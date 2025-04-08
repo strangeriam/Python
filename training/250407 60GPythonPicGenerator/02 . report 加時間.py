de94d11a7953cbebd038a899b4a8fa4bf245cdb0
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 10:54:50 +0800

    Update 02 . report 加時間.py

--- training/250407 60GPythonPicGenerator/02 . report 加時間.py
+++ training/250407 60GPythonPicGenerator/02 . report 加時間.py
@@ -2,5 +2,8 @@ import datetime
 nowDate = datetime.datetime.now().strftime('%Y%m%d')
 nowTime = datetime.datetime.now().strftime('%H%M%S')
 
-print('Date: ', nowDate)
-print('Time: ', nowTime)
+print('Date: ', nowDate) # Date:  20250408
+print('Time: ', nowTime) # Time:  104957
+
+filename = 'report_' + nowTime + '.txt'
+print(filename) # report_104957.txt
