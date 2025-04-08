1758838eaa61d9e2d9031120f5e1f2e7bb41827b
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 15:39:43 +0800

    Create 03 . 運算子 join() . p236.py

---
+++ book-自動化/Chapter 09 . 讀寫檔案/03 . 運算子 join() . p236.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+homeFolder = r'D:\BeeStation\02_python\coding'
+subFolder = 'spam'
+homeFolder + '\\' + subFolder # 'D:\\BeeStation\\02_python\\coding\\spam'
+
+'\\'.join([homeFolder, subFolder]) # 'D:\\BeeStation\\02_python\\coding\\spam'
