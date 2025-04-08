1e1deec5867ecb84bced7c0830914e2b478c20a0
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 10:51:11 +0800

    Create 04 . 變數 連接 字串 +.py

---
+++ book-基本操作/變數 Variable/04 . 變數 連接 字串 +.py
@@ -0,0 +1,10 @@
+import datetime
+nowDate = datetime.datetime.now().strftime('%Y%m%d')
+nowTime = datetime.datetime.now().strftime('%H%M%S')
+
+print('Date: ', nowDate)
+print('Time: ', nowTime)
+
+filename = 'report_' + nowTime
+
+print(filename)
