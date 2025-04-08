9868ed4d4c05c466a9c15eb5074b7486b9e55321
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 08:28:45 +0800

    Update 07 . 1000 天後的日期 timedelta() . p460.py

--- book-自動化/Chapter 17 . 保持時間 工作排程 和啟動/07 . 1000 天後的日期 timedelta() . p460.py
+++ book-自動化/Chapter 17 . 保持時間 工作排程 和啟動/07 . 1000 天後的日期 timedelta() . p460.py
@@ -9,3 +9,18 @@ dt
 thousanDays = datetime.timedelta(days=1000)
 dt + thousanDays
 輸出: datetime.datetime(2027, 8, 2, 14, 35, 21, 715453)
+
+# 加減乘除
+import datetime
+
+dec31st = datetime.datetime(2024, 11, 5, 14, 40, 0)
+aboutThirtyYears = datetime.timedelta(days=365 * 30)
+
+dec31st
+輸出: datetime.datetime(2024, 11, 5, 14, 40)
+
+dec31st - aboutThirtyYears
+輸出: datetime.datetime(1994, 11, 13, 14, 40)
+
+dec31st - (2 * aboutThirtyYears)
+輸出: datetime.datetime(1964, 11, 20, 14, 40)
