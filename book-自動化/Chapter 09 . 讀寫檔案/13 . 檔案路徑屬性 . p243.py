0e0b169f5eb49657e604280bb438d2c13148f0c2
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-08 16:59:57 +0800

    Update 13 . 檔案路徑屬性 . p243.py

--- book-自動化/Chapter 09 . 讀寫檔案/13 . 檔案路徑屬性 . p243.py
+++ book-自動化/Chapter 09 . 讀寫檔案/13 . 檔案路徑屬性 . p243.py
@@ -1,4 +1,13 @@
 
 # 實作前, 切換工作路徑.
+import os
+os.chdir('D:\\BeeStation\\02_python\\coding')
 
-p = Path()
+from pathlib import Path
+p = Path('D:/BeeStation/02_python/coding/spam.txt')
+p.anchor # 'D:\\'
+p.parent # WindowsPath('D:/BeeStation/02_python/coding')
+p.name # 'spam.txt'
+p.stem # 'spam' --> 基本名稱
+p.suffix # '.txt' --> 副檔名
+p.drive # 'D:' --> 實體儲存裝置
