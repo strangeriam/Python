d8292d1701cd46a46de070b3f1f7a25df9dda703
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 10:55:45 +0800

    Update 03 . 整合目錄和檔名.py

--- training/250407 60GPythonPicGenerator/03 . 整合目錄和檔名.py
+++ training/250407 60GPythonPicGenerator/03 . 整合目錄和檔名.py
@@ -1,22 +1,28 @@
 
 # common.py
-def f_new_dirName():
+def f_new_picture():
 	from pathlib import Path
 	import os, datetime
 
 	nowDate = datetime.datetime.now().strftime('%Y%m%d')
 	nowTime = datetime.datetime.now().strftime('%H%M%S')
 
-	resultPath = Path(Path.cwd(), 'result' + '\\' + nowDate)
-	os.makedirs(resultPath)
+	p = Path(Path.cwd(), 'result' + '\\' + nowDate)
 
-	return resultPath
+	if p.is_dir():
+	  	print('Debug: directory exists')
+	else:
+		print('Debug: no directory')
+		os.makedirs(p)
+
+	fname = 'report_' + nowTime + '.png'
+	pf = Path(Path.cwd(), 'result' + '\\' + nowDate + '\\' + fname)
+
+	return pf
 
 
 # go.py
-from common import f_new_dirName
-filename = f_new_dirName()
-print( 'Return: ', filename )
+from common import f_new_picture
+fname = f_new_picture()
+print('Debug-Lu: fname is', fname)
 
-## Modify sourceCode.
-plt.savefig(filename, dpi=300, bbox_inches='tight')
