ce0387e76150fc45351203f1aca8677caff62e6f
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 08:44:02 +0800

    Update 30 . 檔案讀寫 write_text() & read_text() . pg272.py

--- book-自動化/Chapter 09 . 讀寫檔案/30 . 檔案讀寫 write_text() & read_text() . pg272.py
+++ book-自動化/Chapter 09 . 讀寫檔案/30 . 檔案讀寫 write_text() & read_text() . pg272.py
@@ -11,11 +11,7 @@ Path.cwd() # WindowsPath('D:/BeeStation/02_python/250402_mouseMove4')
 # Beginning
 from pathlib import Path
 
-newP = Path('C:\\Users\\Rlulu\\Dropbox\\12-Office-TryTryLu\\Learning_02_Python\\newDir1.txt')
-newP.write_text('Hello, World')
-輸出:
-12
+p = Path('spam.txt')
+p.write_text('Hello, World') # 12
 
-newP.read_text()
-輸出:
-'Hello, World'
+p.read_text() # 'Hello, World'
