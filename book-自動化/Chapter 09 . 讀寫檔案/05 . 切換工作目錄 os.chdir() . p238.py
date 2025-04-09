6860f69340c5b2c91ce7e3af8989e53882fe208c
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 08:33:08 +0800

    Update 05 . 切換工作目錄 os.chdir() . p238.py

--- book-自動化/Chapter 09 . 讀寫檔案/05 . 切換工作目錄 os.chdir() . p238.py
+++ book-自動化/Chapter 09 . 讀寫檔案/05 . 切換工作目錄 os.chdir() . p238.py
@@ -1,7 +1,5 @@
->>> import os
->>> os.chdir('C:\\Windows\\System32')
+import os
+os.chdir('C:\\Windows\\System32')
 
->>> from pathlib import Path
->>> Path.cwd()
-WindowsPath('C:/Windows/System32')
->>>
+from pathlib import Path
+Path.cwd() # WindowsPath('C:/Windows/System32')
