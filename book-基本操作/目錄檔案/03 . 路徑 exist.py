3628f1556ed6d15ccd86991e90d4d21c0ff6a473
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 10:04:01 +0800

    Update 03 . 路徑 exist.py

--- book-基本操作/目錄檔案/03 . 路徑 exist.py
+++ book-基本操作/目錄檔案/03 . 路徑 exist.py
@@ -1,7 +1,16 @@
 from pathlib import Path
 my_file = Path('D:\\BeeStation\\02_python\\250402_mouseMove4\\hello.txt')
 
+# Way 1
 if my_file.exists():
   print('path exists')
 else:
   print('no path')
+
+# Way 2
+try:
+    my_abs_path = my_file.resolve(strict=True)
+except FileNotFoundError:
+    print('no path')
+else:
+    print('path exists')
