75fe10ae8fe91c65769c9418f64612789cadd4f0
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 08:38:02 +0800

    Update 30 . 檔案讀寫 write_text() & read_text() . pg272.py

--- book-自動化/Chapter 09 . 讀寫檔案/30 . 檔案讀寫 write_text() & read_text() . pg272.py
+++ book-自動化/Chapter 09 . 讀寫檔案/30 . 檔案讀寫 write_text() & read_text() . pg272.py
@@ -1,7 +1,12 @@
-
-## 切換工作目錄
+# 事前準備
+# 1. 切換工作目錄
 import os
 os.chdir('D:\BeeStation\02_python\250402_mouseMove4\')
+
+# 2. 查看目前工作目錄
+from pathlib import Path
+Path.cwd() # WindowsPath('C:/Windows/System32')
+
 
 # Beginning
 from pathlib import Path
