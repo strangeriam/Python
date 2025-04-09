99f707e04e88874c1792ab753b10d2c93d82d6c6
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 09:02:16 +0800

    Update 31 . 開啟檔案 open() . pg272.py

--- book-自動化/Chapter 09 . 讀寫檔案/31 . 開啟檔案 open() . pg272.py
+++ book-自動化/Chapter 09 . 讀寫檔案/31 . 開啟檔案 open() . pg272.py
@@ -6,3 +6,17 @@ os.chdir('D:\\BeeStation\\02_python\\250402_mouseMove4')
 # 2. 查看目前工作目錄
 from pathlib import Path
 Path.cwd() # WindowsPath('D:/BeeStation/02_python/250402_mouseMove4')
+
+# Beginning
+
+# Step 1: 新增一文字檔 hello.txt 於工作目錄 'D:/BeeStation/02_python/250402_mouseMove4'
+# Step 2: 開啟此文字檔.
+# Way 1
+p = Path('D:\\BeeStation\\02_python\\250402_mouseMove4')
+helloFile = open(p / 'hello.txt')
+
+# Way 2: 也可這樣開啟.
+helloFile = open('D:\\BeeStation\\02_python\\250402_mouseMove4\\hello.txt')
+
+# Way 3:
+helloFile = open('D:\\BeeStation\\02_python\\250402_mouseMove4\\hello.txt', 'r')
