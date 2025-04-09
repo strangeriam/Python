928b529edfc8041bd56a2c7a89d82b6a8244f839
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 09:12:20 +0800

    Update and rename 33 . 串列每行 readlines() . p274.py to 33 . 串列每行 readlines() . pg274.py

--- book-自動化/Chapter 09 . 讀寫檔案/33 . 串列每行 readlines() . p274.py
+++
@@ -1,8 +0,0 @@
->>>
->>> newP = open('C:\\Users\\Rlulu\\Dropbox\\12-Office-TryTryLu\\Learning_02_Python\\newDir1.txt')
->>>
->>>
->>> newP.readlines()
-['Papa I love U\n', 'Mom Mom\n', 'Ocean\n', 'Mia\n', 'Nia']
->>>
->>>
---
+++ book-自動化/Chapter 09 . 讀寫檔案/33 . 串列每行 readlines() . pg274.py
@@ -0,0 +1,3 @@
+
+newP = open('C:\\Users\\Rlulu\\Dropbox\\12-Office-TryTryLu\\Learning_02_Python\\newDir1.txt')
+newP.readlines() # ['Papa I love U\n', 'Mom Mom\n', 'Ocean\n', 'Mia\n', 'Nia']
