8a9820be288ba268a9c7c79e63f1fe609ffe0145
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 08:34:18 +0800

    Update 30 . 檔案讀寫 write_text() & read_text() . pg272.py

--- book-自動化/Chapter 09 . 讀寫檔案/30 . 檔案讀寫 write_text() & read_text() . pg272.py
+++ book-自動化/Chapter 09 . 讀寫檔案/30 . 檔案讀寫 write_text() & read_text() . pg272.py
@@ -1,3 +1,9 @@
+
+## 切換工作目錄
+import os
+os.chdir('D:\BeeStation\02_python\250402_mouseMove4\')
+
+# Beginning
 from pathlib import Path
 
 newP = Path('C:\\Users\\Rlulu\\Dropbox\\12-Office-TryTryLu\\Learning_02_Python\\newDir1.txt')
