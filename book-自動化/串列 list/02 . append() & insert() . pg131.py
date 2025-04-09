e51dc221062bc226150f5b080bd91b5b7722e27e
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 14:33:47 +0800

    Create 02 . append() & insert() . pg131.py

---
+++ book-自動化/串列 list/02 . append() & insert() . pg131.py
@@ -0,0 +1,15 @@
+# moose 駝鹿
+
+# 注意不可使用 
+# spam = spam.append('moose')
+# spam = spam.insert(1, 'chicken')
+
+# append() . 把引數 加到串列的尾端.
+spam = ['cat', 'dog', 'bat']
+spam.append('moose')
+spam # ['cat', 'dog', 'bat', 'moose']
+
+# insert() . 把引數 加到 索引 (0 ~) 的位置.
+spam = ['cat', 'dog', 'bat']
+spam.insert(1, 'chicken')
+spam #['cat', 'chicken', 'dog', 'bat']
