e18495e9355c367afec106e8b7bf2f7e4d5348d2
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 08:45:25 +0800

    Create 31 . 開啟檔案 open() . pg272.py

---
+++ book-自動化/Chapter 09 . 讀寫檔案/31 . 開啟檔案 open() . pg272.py
@@ -0,0 +1,8 @@
+# 事前準備
+# 1. 切換工作目錄
+import os
+os.chdir('D:\\BeeStation\\02_python\\250402_mouseMove4')
+
+# 2. 查看目前工作目錄
+from pathlib import Path
+Path.cwd() # WindowsPath('D:/BeeStation/02_python/250402_mouseMove4')
