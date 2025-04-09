e032dc25b5049e448305036397d56fea0700189d
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-09 09:49:40 +0800

    Update 02 . 目錄存在否.py

--- book-基本操作/目錄檔案/02 . 目錄存在否.py
+++ book-基本操作/目錄檔案/02 . 目錄存在否.py
@@ -1,1 +1,9 @@
+# 事前準備: 在 D:\\BeeStation\\02_python\\250402_mouseMove4\\
+
 from pathlib import Path
+fname = Path('D:\\BeeStation\\02_python\\250402_mouseMove4\\hello.txt')
+
+if fname.is_dir():
+  print('directory exists')
+else:
+  print('no directory')
