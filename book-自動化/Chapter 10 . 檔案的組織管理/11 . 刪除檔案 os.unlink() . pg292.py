56edaa142df2dbe3278a776eb6c26fd37bf0c2b3
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-11 09:56:29 +0800

    Update and rename 刪除檔案和資料夾 . pg292.py to 11 . 刪除檔案 os.unlink() . pg292.py

--- book-自動化/Chapter 10 . 檔案的組織管理/11 . 刪除檔案 os.unlink() . pg292.py
+++ book-自動化/Chapter 10 . 檔案的組織管理/11 . 刪除檔案 os.unlink() . pg292.py
@@ -6,4 +6,11 @@ os.chdir('D:\\BeeStation\\02_python\\coding')
 from pathlib import Path
 Path.cwd()
 
+# 刪除 所有副檔名為 .png
 # =================================
+import os
+from pathlib import Path
+
+for filename in Path.cwd().glob('*.png'):
+  os.unlink(filename)
+  # print(filename)
