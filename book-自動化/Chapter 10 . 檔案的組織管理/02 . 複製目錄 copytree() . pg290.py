966344c4580febc097e403fe801e5027c3bc5bb6
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-11 10:30:27 +0800

    Create 02 . 複製目錄 copytree() . pg290.py

---
+++ book-自動化/Chapter 10 . 檔案的組織管理/02 . 複製目錄 copytree() . pg290.py
@@ -0,0 +1,17 @@
+# 語法: shutil.copytree(source, destination)
+
+# 清除 shell
+import os
+os.system('cls||clear')
+
+# 實作前, 切換工作路徑.
+import os
+os.chdir('D:\\BeeStation\\02_python\\coding')
+
+# 查看目前工作目錄
+from pathlib import Path
+Path.cwd()
+
+# =======================================
+
+
