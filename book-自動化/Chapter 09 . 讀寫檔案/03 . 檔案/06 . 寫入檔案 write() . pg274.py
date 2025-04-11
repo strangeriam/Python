7199f915120f4c5db692235ec8c8e49f890d6c4b
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-11 08:56:08 +0800

    Update 06 . 寫入檔案 write() . pg274.py

--- book-自動化/Chapter 09 . 讀寫檔案/03 . 檔案/06 . 寫入檔案 write() . pg274.py
+++ book-自動化/Chapter 09 . 讀寫檔案/03 . 檔案/06 . 寫入檔案 write() . pg274.py
@@ -2,15 +2,18 @@
 import os
 os.chdir('D:\\BeeStation\\02_python\\coding')
 
-# Coding now
+# =================================
+# 寫入新檔
 baconFile = open('bacon.txt', 'w')
 baconFile.write('Hello world!\n') # 13
 baconFile.close()
 
+# 遞增文字至下一行
 baconFile = open('bacon.txt', 'a')
 baconFile.write('Bacon is not a vegetable.') # 25
 baconFile.close()
 
+# 讀取檔案
 baconFile = open('bacon.txt')
 content = baconFile.read()
 baconFile.close()
