814ca1018dbb8683f8a694d397d1673704e17157
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-11 10:42:14 +0800

    Update 02 . 複製目錄 shutil.copytree() . pg290.py

--- book-自動化/Chapter 10 . 檔案的組織管理/02 . 複製目錄 shutil.copytree() . pg290.py
+++ book-自動化/Chapter 10 . 檔案的組織管理/02 . 複製目錄 shutil.copytree() . pg290.py
@@ -13,5 +13,10 @@ from pathlib import Path
 Path.cwd()
 
 # =======================================
+import shutil, os
+from pathlib import Path
 
+p = Path.cwd()
 
+# 將 目錄 folder1 (含內含檔案) 複製到另一個 新目錄 folder_backup.
+shutil.copytree(p / 'folder1', p / 'folder_backup') # WindowsPath('D:/BeeStation/02_python/coding/folder_backup')
