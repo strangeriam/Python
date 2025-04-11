8f2410545a629025dde78d99daf425d412b534cd
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-11 08:52:47 +0800

    Update 06 . 寫入檔案 write() . pg274.py

--- book-自動化/Chapter 09 . 讀寫檔案/03 . 檔案/06 . 寫入檔案 write() . pg274.py
+++ book-自動化/Chapter 09 . 讀寫檔案/03 . 檔案/06 . 寫入檔案 write() . pg274.py
@@ -2,4 +2,21 @@
 import os
 os.chdir('D:\\BeeStation\\02_python\\coding')
 
-baconFile = open('bacon.txt, 'w')
+# Coding now
+baconFile = open('bacon.txt', 'w')
+baconFile.write('Hello world!\n') # 13
+baconFile.close()
+
+baconFile = open('bacon.txt', 'a')
+baconFile.write('Bacon is not a vegetable.') # 25
+baconFile.close()
+
+baconFile = open('bacon.txt')
+content = baconFile.read()
+baconFile.close()
+
+print(content)
+
+# OUTPUT:
+Hello world!
+Bacon is not a vegetable.
