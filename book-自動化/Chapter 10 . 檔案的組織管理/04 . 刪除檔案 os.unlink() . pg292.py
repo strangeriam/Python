bb63cfa8a2d92372b9ea1156b05eac4fbdae820a
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-11 14:00:13 +0800

    Update 04 . 刪除檔案 os.unlink() . pg292.py

--- book-自動化/Chapter 10 . 檔案的組織管理/04 . 刪除檔案 os.unlink() . pg292.py
+++ book-自動化/Chapter 10 . 檔案的組織管理/04 . 刪除檔案 os.unlink() . pg292.py
@@ -14,3 +14,28 @@ from pathlib import Path
 for filename in Path.cwd().glob('*.png'):
   os.unlink(filename)
   # print(filename)
+
+# 刪除 資料夾 result, 但資料夾內必須是空的
+# ======================================
+import os
+from pathlib import Path
+
+fname = Path(Path.cwd(), 'result')
+print('fname:', fname)
+
+os.rmdir(fname)
+
+# 刪除 資料夾 result, 和其內所有檔案和子資料夾
+# =========================================
+import shutil, os
+from pathlib import Path
+
+os.chdir('D:\\BeeStation\\02_python\\coding')
+
+p = Path(Path.cwd(), 'result')
+print('p:', p)
+
+# 如果資料夾存在, 就刪除.
+if p.is_dir() == True:
+	print('msg: path exist.')
+	shutil.rmtree(p)
