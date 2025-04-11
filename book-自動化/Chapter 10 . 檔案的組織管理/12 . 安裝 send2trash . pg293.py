a99204dd9c6da10255645ab3e926a97fd16f64a2
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-11 10:07:18 +0800

    Create 12 . 安裝 send2trash . pg293.py

---
+++ book-自動化/Chapter 10 . 檔案的組織管理/12 . 安裝 send2trash . pg293.py
@@ -0,0 +1,18 @@
+# 安裝 send2trash
+pip install --user send2trash
+
+# Log:
+C:\Users\Rlulu>
+C:\Users\Rlulu>pip install --user send2trash
+Collecting send2trash
+  Downloading Send2Trash-1.8.3-py3-none-any.whl.metadata (4.0 kB)
+Downloading Send2Trash-1.8.3-py3-none-any.whl (18 kB)
+Installing collected packages: send2trash
+  WARNING: The script send2trash.exe is installed in 'C:\Users\Rlulu\AppData\Roaming\Python\Python311\Scripts' which is not on PATH.
+  Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
+Successfully installed send2trash-1.8.3
+
+C:\Users\Rlulu>
+
+# =========================
+import send2trash
