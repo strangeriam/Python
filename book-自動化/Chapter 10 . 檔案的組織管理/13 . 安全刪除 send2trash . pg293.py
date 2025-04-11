0107e05ac7cecc11cbc1e48b4560356445bc495f
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-11 10:12:33 +0800

    Update 13 . 安全刪除 send2trash . pg293.py

--- book-自動化/Chapter 10 . 檔案的組織管理/13 . 安全刪除 send2trash . pg293.py
+++ book-自動化/Chapter 10 . 檔案的組織管理/13 . 安全刪除 send2trash . pg293.py
@@ -1,4 +1,4 @@
-# 目的: 將刪除的檔案先 放到資源回收桶. 還可取回.
+# 目的: 刪除 檔案和資料夾 到資源回收桶. 還可取回.
 
 # 清除 shell
 import os
@@ -15,5 +15,10 @@ Path.cwd()
 # =======================================
 import send2trash
 
+# 新增檔案
 baconFile = open('bacon.txt', 'a')
-baconFile.write('Bacon is not a vegetable.')
+baconFile.write('Bacon is not a vegetable.') # 25
+baconFile.close()
+
+# 將檔案丟到 資源回收桶
+send2trash.send2trash('bacon.txt')
