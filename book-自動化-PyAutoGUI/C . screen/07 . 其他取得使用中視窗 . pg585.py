55eec6e65b6f0b8593edb8f3c889e087d770050e
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-16 08:46:41 +0800

    Update 07 . 其他取得使用中視窗 . pg585.py

--- book-自動化-PyAutoGUI/C . screen/07 . 其他取得使用中視窗 . pg585.py
+++ book-自動化-PyAutoGUI/C . screen/07 . 其他取得使用中視窗 . pg585.py
@@ -1,3 +1,3 @@
 getAllWindow() --> 返回螢幕上, 每個可見視窗的 Window 物件串列.
 getWindowAt(x, y) --> 返回每個在點 (x, y) 的可見視窗 Window 物件串列.
-返回視窗標題列中, 含有 title 字串的可見視窗的 Wondow 的物件串列.
+getWindowWithTitle(title) --> 返回視窗標題列中, 含有 title 字串的可見視窗的 Wondow 的物件串列.
