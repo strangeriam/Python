02aeeed8d2d866b02946060488e03fbc76797b72
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-25 08:41:53 +0800

    Update 06 . pipe 比對多組 '|' . pg216.py

--- book-自動化/Chapter 07 . Regular Expression/06 . pipe 比對多組 '|' . pg216.py
+++ book-自動化/Chapter 07 . Regular Expression/06 . pipe 比對多組 '|' . pg216.py
@@ -1,11 +1,11 @@
 # 第一次比對符合, 當成 Match 物件返回.
-
 import re
 
 heroRegex = re.compile(r'Batman|Tina Fey')
 mo1 = heroRegex.search('Batman and Tina Fey.')
-
-mo1.group() # 'Batman'
+a = mo1.group()
+print(a) # Batman
 
 mo2 = heroRegex.search('Tina Fey and Batman.')
-mo2.group() # 'Tina Fey'
+b = mo2.group()
+print(b) # Tina Fey
