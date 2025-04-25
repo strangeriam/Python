9263d6500ec1f8c6c324392e8b36b78287bee463
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-25 09:13:25 +0800

    Update 09 . 選擇性比對 ? . pg217.py

--- book-自動化/Chapter 07 . Regular Expression/09 . 選擇性比對 ? . pg217.py
+++ book-自動化/Chapter 07 . Regular Expression/09 . 選擇性比對 ? . pg217.py
@@ -1,9 +1,9 @@
 import re
 
-batRegex = re.compile(r'Bat(wo)man')
+batRegex = re.compile(r'Bat(wo)?man')
 mo1 = batRegex.search('The Adventures of Batman')
 a = mo1.group()
-print(a)
+print(a) # Batman
 
 mo2 = batRegex.search('The Adventures of Batwoman')
 b = mo2.group()
