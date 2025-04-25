a39434d43922742de9666cbac118705873ef374d
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-25 09:44:03 +0800

    Update 10 . 選擇性比對 '?' . pg218.py

--- book-自動化/Chapter 07 . Regular Expression/10 . 選擇性比對 '?' . pg218.py
+++ book-自動化/Chapter 07 . Regular Expression/10 . 選擇性比對 '?' . pg218.py
@@ -5,12 +5,9 @@ import re
 
 phoneRegex = re.compile(r'(\d\d\d-)?\d\d\d-\d\d\d\d')
 mo1 = phoneRegex.search('My number is 415-555-4242')
-mo1.group()
-輸出:
-'415-555-4242'
+a = mo1.group()
+print(a) # 415-555-4242
 
 mo2 = phoneRegex.search('My number is 555-4242')
-mo2.group()
-輸出:
-'555-4242'
-
+b = mo2.group()
+print(b) # 555-4242
