e3551a92cd74dd075bf464c72f21705d6ee91a7f
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-25 10:18:03 +0800

    Update 11 . 選擇性比對 '*' . pg218.py

--- book-自動化/Chapter 07 . Regular Expression/11 . 選擇性比對 '*' . pg218.py
+++ book-自動化/Chapter 07 . Regular Expression/11 . 選擇性比對 '*' . pg218.py
@@ -1,26 +1,16 @@
-# 正規表示式 (wo)* 比對 wo 的結果為 0.
-## ========================================
+
 import re
 
 batRegex = re.compile(r'Bat(wo)*man')
-mo1 = batRegex.search('The Adventures of Batman')
+
+mo1 = batRegex.search('The Adventures of Batman') # # 正規表示式 (wo)* 比對 wo 的結果為 0.
 a = mo1.group()
 print(a) # Batman
 
-# 正規表示式 (wo)* 比對 wo 的結果為 1.
-## ========================================
-import re
-
-batRegex = re.compile(r'Bat(wo)*man')
-mo2 = batRegex.search('The Adventures of Batwoman')
+mo2 = batRegex.search('The Adventures of Batwoman') # # 正規表示式 (wo)* 比對 wo 的結果為 1.
 b = mo2.group()
 print(b) # Batwoman
 
-# 正規表示式 (wo)* 比對 wo 的結果為 4.
-## ========================================
-import re
-
-batRegex = re.compile(r'Bat(wo)*man')
-mo3 = batRegex.search('The Adventures of Batwowowowoman')
+mo3 = batRegex.search('The Adventures of Batwowowowoman') # 正規表示式 (wo)* 比對 wo 的結果為 4.
 c = mo3.group()
 print(c) # Batwowowowoman
