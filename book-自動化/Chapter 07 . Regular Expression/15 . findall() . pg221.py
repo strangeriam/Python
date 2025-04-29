ef370ffa0916577b38ccb12542eb9cc8fbd59faf
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-29 08:52:03 +0800

    Update 15 . findall() . pg221.py

--- book-自動化/Chapter 07 . Regular Expression/15 . findall() . pg221.py
+++ book-自動化/Chapter 07 . Regular Expression/15 . findall() . pg221.py
@@ -1,6 +1,19 @@
-import re
 
+# ================
+import re
 phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 mo = phoneNumRegex.search('Cell: 415-555-9999 Work: 212-555-0000')
 a = mo.group()
 print(a) # 415-555-9999
+
+# ================
+import re
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') #--> has no groups
+b = phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000')
+print(b) # ['415-555-9999', '212-555-0000']
+
+# ================
+import re
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+c = phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000') #--> has groupss
+print(c) # [('415', '555', '9999'), ('212', '555', '0000')]
