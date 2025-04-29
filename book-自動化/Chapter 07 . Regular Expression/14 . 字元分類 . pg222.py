e190c9bef6374a3903246920ba3addbe93e6eef6
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-04-29 08:31:18 +0800

    Update and rename 00 . 字元分類 . pg222.py to 14 . 字元分類 . pg222.py

--- book-自動化/Chapter 07 . Regular Expression/14 . 字元分類 . pg222.py
+++ book-自動化/Chapter 07 . Regular Expression/14 . 字元分類 . pg222.py
@@ -8,3 +8,14 @@
 
 [0-5] . 只比對 數字 0 到 5.
 [a-zA-Z] . 僅比對字母.
+
+# ===========================================
+import re
+
+xmasRegex = re.compile(r'\d+\s\w+')
+d = xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge')
+
+print(d)
+
+# OUTPUT:
+['12 drummers', '11 pipers', '10 lords', '9 ladies', '8 maids', '7 swans', '6 geese', '5 rings', '4 birds', '3 hens', '2 doves', '1 partridge']
