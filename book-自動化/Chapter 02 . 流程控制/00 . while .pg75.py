c1286a6b093d72a6a448f4557a50d880e9f67521
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-05 15:38:38 +0800

    Update and rename blank to 00 . while .pg75.py

---
+++ book-自動化/Chapter 02 . 流程控制/00 . while .pg75.py
@@ -0,0 +1,21 @@
+spam = 0
+if spam < 5:
+  print('Hello, world.')
+  spam = spam + 1
+
+# 輸出
+Hello, world.
+
+
+spam = 0
+while spam < 5:
+  print('Hello, world.')
+  spam = spam + 1
+
+# 輸出
+Hello, world.
+Hello, world.
+Hello, world.
+Hello, world.
+Hello, world.
+[Finished in 110ms]
