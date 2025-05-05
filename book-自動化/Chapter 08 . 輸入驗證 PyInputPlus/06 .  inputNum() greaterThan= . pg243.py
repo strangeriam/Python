2f2e07f09cc0d2cd742899876c6300ef42690b68
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-05 09:36:05 +0800

    Create 06 .  inputNum() greaterThan= . pg243.py

---
+++ book-自動化/Chapter 08 . 輸入驗證 PyInputPlus/06 .  inputNum() greaterThan= . pg243.py
@@ -0,0 +1,6 @@
+>>> import pyinputplus as pyip
+
+>>> response = pyip.inputNum('Enter num: ', greaterThan=4)
+Enter num: # 輸入 4 --> Number must be greater than 4.
+Enter num: # 輸入 5
+>>> response # 5
