f19dd1fdb8c4a291b780e1abff3eb61816755988
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-05 09:42:17 +0800

    Update 07 . inputNum() lessThan= . pg243.py

--- book-自動化/Chapter 08 . 輸入驗證 PyInputPlus/07 . inputNum() lessThan= . pg243.py
+++ book-自動化/Chapter 08 . 輸入驗證 PyInputPlus/07 . inputNum() lessThan= . pg243.py
@@ -1,4 +1,8 @@
 >>> import pyinputplus as pyip
 
 >>> response = pyip.inputNum('>', min=4, lessThan=6)
-Enter num: # 輸入 6 --> Number must be greater than 4.
+> # 輸入 6 --> Number must be less than 6.
+> # 輸入 3 --> Number must be at minimum 4.
+> # 輸入 4 
+>>>
+>>> response # 4
