22d3ee2a2e17f1ee8fbb5403c098272e6ad1d73c
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-05 10:40:04 +0800

    Update 13 . inputCustom() . pg246.py

--- book-自動化/Chapter 08 . 輸入驗證 PyInputPlus/13 . inputCustom() . pg246.py
+++ book-自動化/Chapter 08 . 輸入驗證 PyInputPlus/13 . inputCustom() . pg246.py
@@ -1,3 +1,4 @@
+import time
 import pyinputplus as pyip
 
 def addsUpToTen(numbers):
@@ -10,3 +11,11 @@ def addsUpToTen(numbers):
 	return int(numbers) #Return an int form of numbers.
 
 response = pyip.inputCustom(addsUpToTen) # No parentheses after addsUptoTen here.
+
+# 輸入 Enter --> Blank values are not allowed.
+# 輸入 123 --> The digits must add up to 10, not 6.
+# 輸入 1235 --> The digits must add up to 10, not 11.
+# 輸入  1234 --> 
+
+print('response: ', response) # response:  1234
+time.sleep(3)
