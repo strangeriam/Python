c833712edf590ba92d2ce14d95fa88475dc6ad4d
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-05 16:43:18 +0800

    Create 04 . print() . pg103.py

---
+++ book-自動化/ Chapter 03 . 函式/04 . print() . pg103.py
@@ -0,0 +1,11 @@
+print('Hello') # Hello
+print('World') # World
+
+# end='' 將換行符號, 換成別的字串.
+print('Hello', end='') # Hello
+print('World') # HelloWorld
+
+print('cats', 'dogs', 'mice') # cats dogs mice
+
+# sep=',' 將預設空格, 改成分隔符號.
+print('cats', 'dogs', 'mice', sep=',') # cats,dogs,mice
