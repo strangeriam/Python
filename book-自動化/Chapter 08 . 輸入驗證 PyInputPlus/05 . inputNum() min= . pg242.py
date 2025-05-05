d52fadd07c2abf7e6766e5860059112678cf3132
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-05 09:36:25 +0800

    Update 05 . inputNum() min= . pg242.py

--- book-自動化/Chapter 08 . 輸入驗證 PyInputPlus/05 . inputNum() min= . pg242.py
+++ book-自動化/Chapter 08 . 輸入驗證 PyInputPlus/05 . inputNum() min= . pg242.py
@@ -1,6 +1,6 @@
-import pyinputplus as pyip
+>>> import pyinputplus as pyip
 
-response = pyip.inputNum('Enter num: ', min=4)
+>>> response = pyip.inputNum('Enter num: ', min=4)
 Enter num: # 輸入 3 --> Number must be at minimum 4.
 Enter num: # 輸入 4
-response # 4
+>>> response # 4
