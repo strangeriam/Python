fae89f87e47eeaa7e77a13bee873696a25a3fd72
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-07 08:39:05 +0800

    Create install pyserial.py

---
+++ boot-特殊環境建立/250507 . Uart/install pyserial.py
@@ -0,0 +1,17 @@
+pip install pyserial
+
+Log:
+Microsoft Windows [版本 10.0.19045.5737]
+(c) Microsoft Corporation. 著作權所有，並保留一切權利。
+
+C:\Users\Rlulu>pip install pyserial
+Collecting pyserial
+  Downloading pyserial-3.5-py2.py3-none-any.whl.metadata (1.6 kB)
+Downloading pyserial-3.5-py2.py3-none-any.whl (90 kB)
+Installing collected packages: pyserial
+Successfully installed pyserial-3.5
+
+[notice] A new release of pip is available: 25.0.1 -> 25.1.1
+[notice] To update, run: C:\Users\Rlulu\AppData\Local\Programs\Python\Python311\python.exe -m pip install --upgrade pip
+
+C:\Users\Rlulu>
