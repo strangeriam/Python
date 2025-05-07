e91eb08464971342f817effd5edefac2a96603fa
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-07 10:31:30 +0800

    Update serial.Close.py

--- boot-特殊環境建立/250507 . Uart/serial.Close.py
+++ boot-特殊環境建立/250507 . Uart/serial.Close.py
@@ -1,6 +1,16 @@
 import serial
 ser = serial.Serial("COM3", 115200)
+
+ser
+#輸出:
+Serial<id=0x157a0c6b280, open=True>(port='COM3', baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
+
 ser.close()
+#輸出:
+Serial<id=0x157a0c6b280, open=False>(port='COM3', baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
+
+
+
 
 import serial
 print('A')
