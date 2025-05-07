58937dac82e2d3f522708554d826ef4b30bce71e
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-07 09:35:46 +0800

    Update close serial port.py

--- boot-特殊環境建立/250507 . Uart/close serial port.py
+++ boot-特殊環境建立/250507 . Uart/close serial port.py
@@ -2,3 +2,19 @@ import serial
 ser = serial.Serial("COM3", 115200)
 
 ser.close()
+
+
+import serial
+
+print('A')
+
+try:
+	ser = serial.Serial("com3", 115200)
+	print('B')
+
+except serial.SerialException:
+	serial.Serial("com3", 115200).close()
+	ser = serial.Serial("com3",115200)
+	print('C')
+
+print('done')
