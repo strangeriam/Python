71ba8542cb342437692f735d260f888da2cbf856
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-12 09:28:51 +0800

    Create 99 . 模組 beautifulshop4 . pg343.py

---
+++ book-自動化/Chapter 12 . 從 Web 擷取資訊/99 . 模組 beautifulshop4 . pg343.py
@@ -0,0 +1,24 @@
+
+# 安裝 beautifulsoup4
+pip install --user beautifulsoup4
+
+import bs4
+
+# ===================================================
+C:\Users\Rlulu>pip install --user beautifulsoup4
+Collecting beautifulsoup4
+  Downloading beautifulsoup4-4.13.4-py3-none-any.whl.metadata (3.8 kB)
+Collecting soupsieve>1.2 (from beautifulsoup4)
+  Downloading soupsieve-2.7-py3-none-any.whl.metadata (4.6 kB)
+Collecting typing-extensions>=4.0.0 (from beautifulsoup4)
+  Downloading typing_extensions-4.13.2-py3-none-any.whl.metadata (3.0 kB)
+Downloading beautifulsoup4-4.13.4-py3-none-any.whl (187 kB)
+Downloading soupsieve-2.7-py3-none-any.whl (36 kB)
+Downloading typing_extensions-4.13.2-py3-none-any.whl (45 kB)
+Installing collected packages: typing-extensions, soupsieve, beautifulsoup4
+Successfully installed beautifulsoup4-4.13.4 soupsieve-2.7 typing-extensions-4.13.2
+
+[notice] A new release of pip is available: 25.0.1 -> 25.1.1
+[notice] To update, run: C:\Users\Rlulu\AppData\Local\Programs\Python\Python311\python.exe -m pip install --upgrade pip
+
+C:\Users\Rlulu>
