03293399eb4f4e3b60a51cc66646853e1e9a3205
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-12 09:20:50 +0800

    Create 99 . 模組 requests 從 Web 下載檔案 . pg333.py

---
+++ book-自動化/Chapter 12 . 從 Web 擷取資訊/99 . 模組 requests 從 Web 下載檔案 . pg333.py
@@ -0,0 +1,33 @@
+# 安裝 requests.
+
+pip install --user requests
+
+## ================================================
+Microsoft Windows [版本 10.0.19045.5737]
+(c) Microsoft Corporation. 著作權所有，並保留一切權利。
+
+C:\Users\Rlulu>pip install --user requests
+Collecting requests
+  Downloading requests-2.32.3-py3-none-any.whl.metadata (4.6 kB)
+Collecting charset-normalizer<4,>=2 (from requests)
+  Downloading charset_normalizer-3.4.2-cp311-cp311-win_amd64.whl.metadata (36 kB)
+Collecting idna<4,>=2.5 (from requests)
+  Downloading idna-3.10-py3-none-any.whl.metadata (10 kB)
+Collecting urllib3<3,>=1.21.1 (from requests)
+  Downloading urllib3-2.4.0-py3-none-any.whl.metadata (6.5 kB)
+Collecting certifi>=2017.4.17 (from requests)
+  Downloading certifi-2025.4.26-py3-none-any.whl.metadata (2.5 kB)
+Downloading requests-2.32.3-py3-none-any.whl (64 kB)
+Downloading certifi-2025.4.26-py3-none-any.whl (159 kB)
+Downloading charset_normalizer-3.4.2-cp311-cp311-win_amd64.whl (105 kB)
+Downloading idna-3.10-py3-none-any.whl (70 kB)
+Downloading urllib3-2.4.0-py3-none-any.whl (128 kB)
+Installing collected packages: urllib3, idna, charset-normalizer, certifi, requests
+  WARNING: The script normalizer.exe is installed in 'C:\Users\Rlulu\AppData\Roaming\Python\Python311\Scripts' which is not on PATH.
+  Consider adding this directory to PATH or, if you prefer to suppress this warning, use --no-warn-script-location.
+Successfully installed certifi-2025.4.26 charset-normalizer-3.4.2 idna-3.10 requests-2.32.3 urllib3-2.4.0
+
+[notice] A new release of pip is available: 25.0.1 -> 25.1.1
+[notice] To update, run: C:\Users\Rlulu\AppData\Local\Programs\Python\Python311\python.exe -m pip install --upgrade pip
+
+C:\Users\Rlulu>
