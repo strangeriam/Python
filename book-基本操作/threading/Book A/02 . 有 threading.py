cf91740d95dfb1083a4f68527b8984ad1e56f568
Rlulu <64244191+strangeriam@users.noreply.github.com>
2025-05-12 10:06:59 +0800

    Create 02 . 有 threading.py

---
+++ book-基本操作/threading/Book A/02 . 有 threading.py
@@ -0,0 +1,35 @@
+import threading, time
+
+def aa():
+    i = 0
+    while i<5:
+        i = i + 1
+        time.sleep(0.5)
+        print('A:', i)
+
+def bb():
+    i = 0
+    while i<50:
+        i = i + 10
+        time.sleep(0.5)
+        print('B:', i)
+
+a = threading.Thread(target=aa)  # 建立新的執行緒
+b = threading.Thread(target=bb)  # 建立新的執行緒
+
+a.start()  # 啟用執行緒
+b.start()  # 啟用執行緒
+
+
+# 輸出:
+A: 1
+B: 10
+A: 2
+B: 20
+A: 3
+B: 30
+A: 4
+B: 40
+B:A: 5
+ 50
+[Finished in 2.6s]
