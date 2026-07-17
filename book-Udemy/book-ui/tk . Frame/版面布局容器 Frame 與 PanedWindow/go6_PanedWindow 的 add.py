ef8f8d471087a211de2b5b5079a0370ea53c67ca
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-17 14:05:47 +0800

    Update and rename go6.py to go6_PanedWindow 的 add.py

--- book-Udemy/book-ui/tk . Frame/版面布局容器 Frame 與 PanedWindow/go6_PanedWindow 的 add.py
+++ book-Udemy/book-ui/tk . Frame/版面布局容器 Frame 與 PanedWindow/go6_PanedWindow 的 add.py
@@ -1,6 +1,5 @@
-測試 6 : 使用 PanedWindow 的 add() 方法將元件加入容器 
-
-結果與上面範例相同. 
+# 使用 PanedWindow 的 add() 方法將元件加入容器 
+# 結果與上面範例相同. 
 
 import tkinter as tk
 from tkinter import ttk
@@ -24,6 +23,7 @@ account=tk.Entry(pw, width=10)  # 帳號輸入框
 pw.add(account)  # 元件放入視窗
 password=tk.Entry(pw, width=10, show='*')  # 密碼輸入框
 pw.add(password)  # 元件放入視窗
+
 login_btn=tk.Button(pw, text='登入', command=login)  # 登入按鈕
 pw.add(login_btn)  # 元件放入視窗
 placeholder=tk.Label(pw, width=10)  # 佔位用的 Label
@@ -32,6 +32,7 @@ command=tk.Entry(pw, width=20)  # 指令輸入框
 pw.add(command)  # 元件放入視窗
 run_btn=tk.Button(pw, text='執行', command=run)  # 登入按鈕
 pw.add(run_btn)  # 元件放入視窗
+
 # 建立執行結果文字區域
 result=tk.Text(root)
 result.pack(fill=tk.BOTH, expand=True)
