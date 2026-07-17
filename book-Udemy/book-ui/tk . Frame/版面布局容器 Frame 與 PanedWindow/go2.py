b2e90977558b819919b578d3c43b45994b55e3f9
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-17 10:55:54 +0800

    Update go2.py

--- book-Udemy/book-ui/tk . Frame/版面布局容器 Frame 與 PanedWindow/go2.py
+++ book-Udemy/book-ui/tk . Frame/版面布局容器 Frame 與 PanedWindow/go2.py
@@ -25,17 +25,23 @@ root.geometry('800x600')  # 設定視窗尺寸
 # 建立登入介面與指令輸入介面
 frame=tk.Frame(root)  # 外部 Frame 元件
 frame.pack()
+
 # 建立以 Frame 為父容器的元件
 account=tk.Entry(frame, width=10)  # 帳號輸入框
 account.pack(side=tk.LEFT)  # 元件放入視窗
+
 password=tk.Entry(frame, width=10, show='*')  # 密碼輸入框
 password.pack(side=tk.LEFT)  # 元件放入視窗
+
 login_btn=tk.Button(frame, text='登入', command=login)  # 登入按鈕
 login_btn.pack(side=tk.LEFT)  # 元件放入視窗
+
 command=tk.Entry(frame, width=20)  # 指令輸入框
 command.pack(side=tk.LEFT)  # 元件放入視窗
+
 run_btn=tk.Button(frame, text='執行', command=run)  # 登入按鈕
 run_btn.pack(side=tk.LEFT)  # 元件放入視窗
+
 # 建立執行結果文字區域
 result=tk.Text(root)
 result.pack(fill=tk.BOTH, expand=True)
