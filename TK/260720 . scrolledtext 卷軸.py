b7ef21de94e2fa38378d72fa5ad10f8db85fddcb
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-20 17:25:52 +0800

    Update 260720 . scrolledtext 卷軸.py

--- TK/260720 . scrolledtext 卷軸.py
+++ TK/260720 . scrolledtext 卷軸.py
@@ -2,15 +2,11 @@ import tkinter as tk
 from tkinter import scrolledtext, messagebox
 
 root = tk.Tk()
-root.geometry('400x500')
 
-frameMID1 = tk.Frame(root)
-frameMID1.pack()
-
-lblEvent = tk.Label(frameMID1, text='Event Log:')
+lblEvent = tk.Label(root, text='Event Log:')
 lblEvent.pack()
 
-logArea = scrolledtext.ScrolledText(frameMID1, width=60, height=25, state=tk.DISABLED)
+logArea = scrolledtext.ScrolledText(root, width=60, height=25, state=tk.DISABLED)
 logArea.pack(padx=10, pady=5)
 
 root.mainloop()
