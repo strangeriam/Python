3fd9e7d7d7edc1c092e2859d23a9f36f81e8e4d5
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-20 14:43:43 +0800

    Update 260720a . button.py

--- TK/260720a . button.py
+++ TK/260720a . button.py
@@ -1,9 +1,16 @@
 import tkinter as tk
+
 root = tk.Tk()
 
 def _f_showPort():
-	print('this is pront...')
+	print('This is _f_showPort.')
 
-btn_com = tk.Button(root, text='connect', command=_f_showPort)
+btn_com = tk.Button(root, text="connect", command=_f_showPort, width=15)
 btn_com.pack(pady=10)
-btn_com.config(NORMAL)
+btn_com.config(state=tk.DISABLED)
+
+root.mainloop()
+
+# =======================
+state=tk.DISABLED <-- Button 不給點選
+state=tk.NORMAL <-- Button 開放點選
