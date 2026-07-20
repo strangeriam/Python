795d4ea9ffd2439ddac82d9dcd43dc52ea90daf0
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-20 14:28:53 +0800

    Update 260720a . button.py

--- TK/260720a . button.py
+++ TK/260720a . button.py
@@ -1,3 +1,9 @@
 import tkinter as tk
-
 root = tk.Tk()
+
+def _f_showPort():
+	print('this is pront...')
+
+btn_com = tk.Button(root, text='connect', command=_f_showPort)
+btn_com.pack(pady=10)
+btn_com.config(NORMAL)
