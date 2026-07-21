1e620e6360b24cb50e376de56093e122e93ce1fd
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-21 16:47:01 +0800

    Update 260721a . Entry 的值轉換成變數.py

--- TK/260721a . Entry 的值轉換成變數.py
+++ TK/260721a . Entry 的值轉換成變數.py
@@ -3,16 +3,13 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry('500x200')
 
-def _f_change_port():
-	var_portNum.set('5')
+def f_change():
+	a.set('5')
 
-var_portNum = tk.StringVar()
-var_portNum.set('3')
+a = tk.StringVar()
+a.set('3')
 
-entry_com = tk.Entry(root, textvariable=var_portNum, state=tk.NORMAL)
-entry_com.pack()
-
-btn_com_connect = tk.Button(root, text='connect', command=_f_change_port)
-btn_com_connect.pack(pady=10)
+entry1 = tk.Entry(root, textvariable=a, state=tk.NORMAL).pack()
+btn1 = tk.Button(root, text='CHANGE', command=f_change).pack(pady=10)
 
 root.mainloop()
