869b6a476eed1ac9eff853c2da598ae95251d5e8
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-22 08:26:22 +0800

    Create 260721a . 全域變數 global.py

---
+++ book-basic-1/variable/260721a . 全域變數 global.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('500x200')
+
+def f_change():
+	global port
+	port = a.get()
+
+a = tk.StringVar()
+a.set('')
+
+port = ''
+
+entry1 = tk.Entry(root, textvariable=a, state=tk.NORMAL)
+entry1.pack()
+
+btn1 = tk.Button(root, text='CHANGE', command=f_change).pack(pady=10)
+
+root.mainloop()
+
+# ==========================
+print(port)
+輸出: ''
+
+在 Entry 輸入 8, 點擊 button "CHANGE"
+print(port)
+輸出: 8
