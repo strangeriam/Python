649d739eddf157169f2941c379a538b4b1ce96f5
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-22 08:46:06 +0800

    Update 260720 . print + variable.py

--- book-basic-1/print/260720 . print + variable.py
+++ book-basic-1/print/260720 . print + variable.py
@@ -4,16 +4,21 @@ city = "Amsterdam"
 country = "Netherlands"
 
 # 傳遞引數的值
-print("City", city, 'is in the country', country) # City Amsterdam is in the country Netherlands
+# City Amsterdam is in the country Netherlands
+print("City", city, 'is in the country', country)
 
 # 格式化字串 (順序傳遞)
-print("City {} is in the country {}".format(city, country)) # City Amsterdam is in the country Netherlands
+# City Amsterdam is in the country Netherlands
+print("City {} is in the country {}".format(city, country))
 
 # 格式化字串 (建立數字索引)
-print("City {1} is in the country {0}, yes, in {0}".format(country, city)) # City Amsterdam is in the country Netherlands, yes, in Netherlands
+# City Amsterdam is in the country Netherlands, yes, in Netherlands
+print("City {1} is in the country {0}, yes, in {0}".format(country, city))
  
 # 格式化字串 (給每個引用命名)
-print("City {city} is in the country {country}".format(country=country, city=city)) # City Amsterdam is in the country Netherlands
+# City Amsterdam is in the country Netherlands
+print("City {city} is in the country {country}".format(country=country, city=city))
 
 # 多個引數組成元組來傳遞引數
-print("City %s is in the country %s" %(city, country)) # City Amsterdam is in the country Netherlands
+# City Amsterdam is in the country Netherlands
+print("City %s is in the country %s" %(city, country))
