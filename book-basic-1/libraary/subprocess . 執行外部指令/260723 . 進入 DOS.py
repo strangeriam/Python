d2ba82cc8322a30bb60bb918897fabd9a5491160
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-23 08:51:59 +0800

    Create 260723 . 進入 DOS.py

---
+++ book-basic-1/libraary/subprocess . 執行外部指令/260723 . 進入 DOS.py
@@ -0,0 +1,36 @@
+
+>>> import subprocess
+>>> subprocess.run(['cmd'])
+Microsoft Windows [版本 10.0.26200.8875]
+(c) Microsoft Corporation. 著作權所有，並保留一切權利。
+
+C:\Users\Rlulu\AppData\Local\Programs\Python\Python311>dir
+ 磁碟區 C 中的磁碟沒有標籤。
+ 磁碟區序號:  70EF-7C16
+
+ C:\Users\Rlulu\AppData\Local\Programs\Python\Python311 的目錄
+
+2025/04/11  上午 09:14    <DIR>          .
+2023/04/07  下午 04:18    <DIR>          ..
+2023/04/07  下午 04:19    <DIR>          DLLs
+2023/04/07  下午 04:19    <DIR>          Doc
+2025/02/12  上午 10:03         2,300,175 get-pip.py
+2023/04/07  下午 04:18    <DIR>          include
+2023/04/07  下午 04:19    <DIR>          Lib
+2023/04/07  下午 04:19    <DIR>          libs
+2023/04/05  上午 12:04            32,768 LICENSE.txt
+2023/04/05  上午 12:05         1,481,302 NEWS.txt
+2023/04/05  上午 12:04           103,192 python.exe
+2023/04/05  上午 12:04            67,344 python3.dll
+2023/04/05  上午 12:04         5,760,784 python311.dll
+2023/04/05  上午 12:04           101,648 pythonw.exe
+2026/07/22  下午 01:59    <DIR>          Scripts
+2025/04/02  下午 04:42    <DIR>          share
+2023/04/07  下午 04:19    <DIR>          tcl
+2023/04/07  下午 04:19    <DIR>          Tools
+2023/04/05  上午 12:04           109,392 vcruntime140.dll
+2023/04/05  上午 12:04            49,520 vcruntime140_1.dll
+               9 個檔案      10,006,125 位元組
+              11 個目錄  72,073,904,128 位元組可用
+
+C:\Users\Rlulu\AppData\Local\Programs\Python\Python311>
