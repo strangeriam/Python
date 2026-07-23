190a161088880a67901e0cb67fb71a76ec5b3cd4
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-23 10:36:07 +0800

    Update 260723e . subprocess 參數.py

--- book-basic-1/subprocess . 執行外部指令/260723e . subprocess 參數.py
+++ book-basic-1/subprocess . 執行外部指令/260723e . subprocess 參數.py
@@ -7,7 +7,7 @@ subprocess.run(['pwd'], cwd='/Users')
 
 
 
-# 未知的使用
+# 未知如何使用
 check :
 希望在外部指令執行發生錯誤時, 讓 Python 也跟著拋出例外 (exception),
 可設定 check 參數為 True, 讓 Python 檢查指令的回傳碼 (returncode), 只要不是 0, 就會拋出錯誤.
