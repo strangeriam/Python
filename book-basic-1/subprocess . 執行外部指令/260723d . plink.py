8df3188fd7ba947ff8ce9d42105030703c1ee132
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-23 09:37:14 +0800

    Update 260723d . plink.py

--- book-basic-1/subprocess . 執行外部指令/260723d . plink.py
+++ book-basic-1/subprocess . 執行外部指令/260723d . plink.py
@@ -1,5 +1,6 @@
 import subprocess
 
 command = 'D:\worktmp\plink.exe'
-subprocess.run([command, '-ssh', '192.168.2.1'], capture_output=True, text=True)
+r = subprocess.run([command, '-ssh', '192.168.2.1'], capture_output=True, text=True)
 
+print(r.stdout)
