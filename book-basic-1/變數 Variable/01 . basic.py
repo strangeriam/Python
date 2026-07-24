fade8bbebd9408464542b00a861fb982fbf7318b
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-24 16:38:26 +0800

    Update 01 . basic.py

--- book-basic-1/變數 Variable/01 . basic.py
+++ book-basic-1/變數 Variable/01 . basic.py
@@ -1,10 +1,8 @@
 
 aaa = 40
-aaa
 # output: 40
 
 bbb = aaa + 2
-bbb
 # output: 42
 
 ccc = 30
@@ -12,10 +10,9 @@ aaa + ccc
 # output: 70
 
 ccc = aaa + ccc
-ccc
 # output: 70
 
-帶入變數的是字串, 就要用 '' 括起來.
+# 帶入變數的是字串, 就要用 '' 括起來.
 aaa = 'hello'
 
 # 跳出錯誤訊息
@@ -26,6 +23,4 @@ NameError: name 'hello' is not defined. Did you mean: 'help'?
 >>>
 
 aaa = 'hello'
-aaa
 # output: 'hello'
-
