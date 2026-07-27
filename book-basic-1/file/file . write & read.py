30baa018154ff72afba10da9a3891f0a3a702818
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-27 16:55:22 +0800

    Update file . write & read.py

--- book-basic-1/file/file . write & read.py
+++ book-basic-1/file/file . write & read.py
@@ -5,7 +5,13 @@ Path.cwd()
 輸出:
 WindowsPath('C:/Users/Rlulu/AppData/Local/Programs/Python/Python311')
 
-p = Path('spam.txt')
-p.write_text('Hello, World') # 12
+# 切換工作目錄 
+import os
+os.chdir('D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling')
 
-p.read_text() # 'Hello, World'
+# 寫入檔案
+fname = Path('hello.log')
+fname.write_text('Hello, World')
+
+# 讀取檔案
+fname.read_text()
