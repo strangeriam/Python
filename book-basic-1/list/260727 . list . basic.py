bc03fb7c5bca4d2a30b788e9897b2b6ddfbfd545
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-27 14:31:09 +0800

    Update 260727 . list . basic.py

--- book-basic-1/list/260727 . list . basic.py
+++ book-basic-1/list/260727 . list . basic.py
@@ -1,12 +1,15 @@
-num1 = 48
-num2 = 8
 
-num_start = int(-num1)
-num_end = int(num1) + int(num2)
+listitem = ['aa', 'bb', 'cc']
+listitem
 
-num_list = []
-for i in range(num_start, num_end, num2):
-	num_list.append(i)
+輸出: 
+['aa', 'bb', 'cc']
 
+# ========================
+listitem[1]
+輸出:
+'bb'
 
-print('num_list: ', num_list) # num_list:  [-48, -40, -32, -24, -16, -8, 0, 8, 16, 24, 32, 40, 48]
+listitem[0]
+輸出:
+'aa'
