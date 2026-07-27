dd7417d0ffb5f0eebe52e848e3b81d18202bfcc0
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-27 15:11:24 +0800

    Update append.py

--- book-basic-1/list/append.py
+++ book-basic-1/list/append.py
@@ -6,3 +6,13 @@ listitem.append('dd')
 listitem
 輸出:
 ['aa', 'bb', 'cc', 'dd']
+
+# ===================
+listitem = ['aa', 'bb', 'cc']
+var == 'hello'
+
+listitem.append(var)
+listitem
+
+輸出:
+['aa', 'bb', 'cc', 'hello']
