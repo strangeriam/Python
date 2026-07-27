4bcf6365c7b75f84a949c2b0c01f4dcf4d3393b3
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-27 15:18:06 +0800

    Create 取消變數 del.py

---
+++ book-basic-1/變數 Variable/取消變數 del.py
@@ -0,0 +1,13 @@
+var = 'aa'
+
+輸出:
+'aa'
+
+#====
+del var
+var
+
+輸出:
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'var' is not defined. Did you mean: 'vars'?
