36db9b3f4608d42a0ad33848d9a9d00d521324bb
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-27 17:04:56 +0800

    Create file . append.py

---
+++ book-basic-1/file/file . append.py
@@ -0,0 +1,21 @@
+import os
+os.chdir('D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling')
+
+# 寫入新檔
+fname = open('hello.log', 'w')
+fname.write('this is line 1.\n')
+fname.close()
+
+# Append 到下一行
+fname = open('hello.log', 'a')
+fname.write('this is line 2.')
+fname.close()
+
+# 讀取檔案
+fname = open('hello.log')
+content = fname.read()
+fname.close()
+
+輸出:
+Hello world!
+Bacon is not a vegetable.
