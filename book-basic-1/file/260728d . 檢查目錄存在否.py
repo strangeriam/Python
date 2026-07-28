0590b2f9866144939267998facf0535368c4fff6
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-28 16:19:46 +0800

    Update 260728d . 檢查目錄存在否.py

--- book-basic-1/file/260728d . 檢查目錄存在否.py
+++ book-basic-1/file/260728d . 檢查目錄存在否.py
@@ -1,3 +1,10 @@
 import os
 
+# 建立新目錄
 os.makedirs('C:\\Users\\Rlulu\\Dropbox\\12-Office-TryTryLu\\Learning_02_Python\\test-dir-241011')
+
+
+# 實驗前先切換到工作區.
+os.chdir('D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling')
+
+from pathlib import Path
