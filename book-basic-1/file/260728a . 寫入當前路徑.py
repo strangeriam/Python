355dfe72736e14c0316d4f660df6f7c712cda4d5
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-28 09:46:29 +0800

    Update 260728a . 寫入當前路徑.py

--- book-basic-1/file/260728a . 寫入當前路徑.py
+++ book-basic-1/file/260728a . 寫入當前路徑.py
@@ -14,6 +14,17 @@ pfile = Path.cwd()
 WindowsPath('D:/Dropbox/14-Office-TryTryLu/Python_01_RS232_RebootCycling')
 
 # Step 3:
+# 建立可 append 的檔案.
+fname = (pfile / 'hello.log')
+appendFile = open(fname, 'a')
 
+# 只寫入 1 行
+appendFile.write('this is line 1.')
+appendFile.close()
 
+# 再寫入 2 行
+appendFile = open(fname, 'a')
+appendFile.write('this is line 2.')
+appendFile.write('this is line 3.')
+appendFile.close()
 
