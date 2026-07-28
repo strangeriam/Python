6dd06cb585ea815ef1a5edde12d785b482573b39
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-28 14:19:40 +0800

    Update 260728b . 建立檔名+時間.py

--- book-basic-1/file/260728b . 建立檔名+時間.py
+++ book-basic-1/file/260728b . 建立檔名+時間.py
@@ -4,5 +4,14 @@ import datetime
 nowTime = datetime.datetime.now().strftime('%H%M%S')
 fname = 'hello_' + nowTime + '.log'
 
-# Way 2
-fname = 'hello_' + (datetime.datetime.now().strftime('%H%M%S')) + '.log'
+# Way 2 + 當前完整工作路徑
+import os
+from pathlib import Path
+
+pfile = Path.cwd()
+filename = 'hello_' + (datetime.datetime.now().strftime('%H%M%S')) + '.log'
+
+fname = pfile / filename
+
+輸出:
+WindowsPath('D:/Dropbox/14-Office-TryTryLu/Python_01_RS232_RebootCycling/hello_141756.log')
