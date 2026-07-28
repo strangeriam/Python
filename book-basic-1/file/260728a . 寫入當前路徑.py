304b3ddfdffb6e109636fef7f801aababbce9f42
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-28 09:04:14 +0800

    Update 260728a . 寫入當前路徑.py

--- book-basic-1/file/260728a . 寫入當前路徑.py
+++ book-basic-1/file/260728a . 寫入當前路徑.py
@@ -1,4 +1,12 @@
 
+# Step 1:
 # 先切換到執行實驗的 工作目錄.
 # 工作目錄: D:\Dropbox\14-Office-TryTryLu\Python_01_RS232_RebootCycling\
+import os
+pfile = 'D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling'
+os.chdir(pfile)
+
+# Step 2:
+# 檢查目前所在目錄.
+
 
