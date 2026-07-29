00b48efd860fd9ea7d7f443c82c9591113073bf6
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-29 09:41:19 +0800

    Update 260720 . def.py

--- book-basic-1/def/260720 . def.py
+++ book-basic-1/def/260720 . def.py
@@ -2,9 +2,7 @@ def sayHello():
   print('hello world')
 
 sayHello()
-
-輸出:
-hello world
+# 輸出: hello world
 
 # 加 return
 def sayHello():
@@ -12,7 +10,12 @@ def sayHello():
   return 1
 
 sayHello()
-
-輸出:
+# 輸出:
 hello world
 1
+
+def hello(name):
+	print('Hello ' + name)
+
+hello('Lu')
+# OUTPUT: Hello Lu
