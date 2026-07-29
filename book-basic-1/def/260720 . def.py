7937e30d1b3fa7db283bd94d7e7285a49578310d
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-29 10:20:28 +0800

    Update 260720 . def.py

--- book-basic-1/def/260720 . def.py
+++ book-basic-1/def/260720 . def.py
@@ -1,10 +1,14 @@
+
+# Way 1: 簡單函數.
+# ====================
 def sayHello():
   print('hello world')
 
 sayHello()
 # 輸出: hello world
 
-# 加 return
+# Way 2: 簡單函數 加 return.
+# ====================
 def sayHello():
   print('hello world')
   return 1
@@ -14,8 +18,21 @@ sayHello()
 hello world
 1
 
+# Way 3: 帶入 1 個變數.
+# ====================
 def hello(name):
 	print('Hello ' + name)
 
 hello('Lu')
 # OUTPUT: Hello Lu
+
+# Way 4: 帶入 多個 變數.
+# ====================
+def hello(dad_mom, name):
+	print('My ' + dad_mom + ' is ' + name + '.')
+
+hello('dad', 'Lu')
+
+輸出:
+My dad is Lu.
+
