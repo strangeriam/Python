643ec8b9d3a12037dac7d395b2b155858c3bf322
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-29 08:33:29 +0800

    Update 260729 . apc.py

--- book-basic-2/APC SNMP/260729 . apc.py
+++ book-basic-2/APC SNMP/260729 . apc.py
@@ -1,4 +1,6 @@
 # APC DOS command: 
+# snmpget -v 1 -c public 192.168.0.200 .1.3.6.1.4.1.17420.1.2.9.1.13.0
+# snmpset -v 1 -c public 192.168.0.200 .1.3.6.1.4.1.17420.1.2.9.1.13.0 s "A,B,-1,-1,-1,-1,-1,-1" 
 
 import subprocess
 
