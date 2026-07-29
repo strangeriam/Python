6f0553e5061fe5559f00209cf50b4c56fc2e169a
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-29 08:49:42 +0800

    Update 260729 . apc.py

--- book-basic-2/APC SNMP/260729 . apc.py
+++ book-basic-2/APC SNMP/260729 . apc.py
@@ -1,6 +1,11 @@
 # APC DOS command: 
-# snmpget -v 1 -c public 192.168.0.200 .1.3.6.1.4.1.17420.1.2.9.1.13.0
-# snmpset -v 1 -c public 192.168.0.200 .1.3.6.1.4.1.17420.1.2.9.1.13.0 s "A,B,-1,-1,-1,-1,-1,-1" 
+# SnmpGet.exe -r:192.168.0.200 -o:.1.3.6.1.4.1.17420.1.2.9.1.13.0
+
+# $SPort / $SPort2 : 1 (ON) 0 (OFF)
+# SnmpSet.exe -r:192.168.0.200 -c:public -o:.1.3.6.1.4.1.17420.1.2.9.1.13.0 -val:$SPort,$PORT2,-1,-1,-1,-1,-1,-1
+
+# Power A & B OFF Both
+# SnmpSet.exe -r:192.168.0.200 -c:public -o:.1.3.6.1.4.1.17420.1.2.9.1.13.0 -val:0,0,-1,-1,-1,-1,-1,-1
 
 import subprocess
 
