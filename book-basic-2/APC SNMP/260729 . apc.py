04783d6674a7bec9e6a3326238b2c8af3fdc6a9f
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-29 08:31:20 +0800

    Update 260729 . apc.py

--- book-basic-2/APC SNMP/260729 . apc.py
+++ book-basic-2/APC SNMP/260729 . apc.py
@@ -1,1 +1,8 @@
-waiting
+# APC DOS command: 
+
+import subprocess
+
+command = 'D:\worktmp\plink.exe'
+r = subprocess.run([command, '-ssh', '192.168.2.1', '-l', 'root'], capture_output=True, text=True)
+
+print(r.stdout)
