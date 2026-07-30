0fdac7658c8677732fb9c5d09c5ad304bcd2ae2c
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-30 13:54:18 +0800

    Update 260730a . label.py

--- book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/55 . Labels in Tkinter/260730a . label.py
+++ book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/55 . Labels in Tkinter/260730a . label.py
@@ -1,7 +1,11 @@
+# set_dpi_awareness() <-- 在 MS-Windows, 防止於高畫素的桌面 失真.
+# 於 Windows 11, 不啟用 set_dpi_awareness(), UI 看起來並無失真, 功能待確認.
+
+
 import tkinter as tk
 from tkinter import ttk
-from windows import set_dpi_awareness
 
+from windows import set_dpi_awareness
 set_dpi_awareness()
 
 root = tk.Tk()
