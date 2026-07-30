b3410073899893c1562d4598a58dd15da04568d5
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-30 11:26:58 +0800

    Create 260730a . code1.py

---
+++ book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/55 . Labels in Tkinter/260730a . code1.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+from windows import set_dpi_awareness
+
+set_dpi_awareness
+
+root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Widget Example")
+
+root.mainloop()
