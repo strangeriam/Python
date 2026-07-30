7b73097c16e621baf4be2fac09e8475d0564feb1
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-30 11:35:58 +0800

    Update 260730a . code1.py

--- book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/55 . Labels in Tkinter/260730a . code1.py
+++ book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/55 . Labels in Tkinter/260730a . code1.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import ttk
 from windows import set_dpi_awareness
 
 set_dpi_awareness()
@@ -8,5 +8,9 @@ root = tk.Tk()
 root.geometry("600x400")
 root.resizable(False, False)
 root.title("Widget Example")
+
+label = ttk.Label(root, text="Hello, world !!!", padding=20)
+label.config(font=("segoe UI", 20))
+label.pack()
 
 root.mainloop()
