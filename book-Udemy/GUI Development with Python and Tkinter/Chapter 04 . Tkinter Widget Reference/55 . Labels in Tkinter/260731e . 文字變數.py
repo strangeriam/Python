277717498f562d65d4034563d35f49b85459b570
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-31 08:45:55 +0800

    Create 260731e . 文字變數.py

---
+++ book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/55 . Labels in Tkinter/260731e . 文字變數.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from windows import set_dpi_awareness
+
+set_dpi_awareness()
+
+root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Widget Example")
+
+
+label.pack()
+
+root.mainloop()
