947051d8322d00a732e8a6b9cf1fcabcffdf1ec2
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-31 08:48:06 +0800

    Update 260731d . textvariable . 文字變數.py

--- book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/55 . Labels in Tkinter/260731d . textvariable . 文字變數.py
+++ book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/55 . Labels in Tkinter/260731d . textvariable . 文字變數.py
@@ -10,7 +10,11 @@ root.geometry("600x400")
 root.resizable(False, False)
 root.title("Widget Example")
 
+greeting = tk.StringVar()
 
+label = ttk.Label(root, padding=10, textvariable=greeting)
 label.pack()
+
+greeting.set("Hello, Lu !")
 
 root.mainloop()
