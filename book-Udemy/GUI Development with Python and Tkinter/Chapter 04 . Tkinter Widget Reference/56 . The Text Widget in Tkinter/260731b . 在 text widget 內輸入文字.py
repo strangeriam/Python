d7be880c2ba071ee39c3e50b3aa524b48e18c194
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-31 09:10:42 +0800

    Update 260731b . 在 text widget 內輸入文字.py

--- book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/56 . The Text Widget in Tkinter/260731b . 在 text widget 內輸入文字.py
+++ book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/56 . The Text Widget in Tkinter/260731b . 在 text widget 內輸入文字.py
@@ -13,4 +13,9 @@ root.title("Widget Example")
 text = tk.Text(root, height=8)
 text.pack()
 
+# 1.0 <-- 文字放的位置.
+# 1 --> 第幾行, 此為 第一行.
+# 0 --> 第幾個字串, 此為第一個字.
+text.insert("1.0", "Please enter a comment...")
+
 root.mainloop()
