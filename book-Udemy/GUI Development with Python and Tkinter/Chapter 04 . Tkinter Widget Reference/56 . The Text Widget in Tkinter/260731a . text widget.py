27669fb5f22b58d6ca908be70185835ea282c997
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-07-31 09:01:18 +0800

    Update 260731a . text widget.py

--- book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/56 . The Text Widget in Tkinter/260731a . text widget.py
+++ book-Udemy/GUI Development with Python and Tkinter/Chapter 04 . Tkinter Widget Reference/56 . The Text Widget in Tkinter/260731a . text widget.py
@@ -1,1 +1,16 @@
-fsfsf
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from windows import set_dpi_awareness
+
+set_dpi_awareness()
+
+root = tk.Tk()
+root.geometry("600x400")
+root.resizable(False, False)
+root.title("Widget Example")
+
+
+
+
+root.mainloop()
