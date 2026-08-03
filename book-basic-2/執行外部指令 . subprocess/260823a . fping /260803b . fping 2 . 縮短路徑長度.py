151ba2368770960fbe86e41b61728655a8ec7f92
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-08-03 15:29:47 +0800

    Update 260803b . fping 2 . 縮短路徑長度.py

--- book-basic-2/執行外部指令 . subprocess/260823a . fping /260803b . fping 2 . 縮短路徑長度.py
+++ book-basic-2/執行外部指令 . subprocess/260823a . fping /260803b . fping 2 . 縮短路徑長度.py
@@ -5,10 +5,10 @@ import os
 import subprocess
 from pathlib import Path
 
-ENVPATH = Path('D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling\\')
+ENVPATH = Path('D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling')
 
 # 然後再進入 fping.exe 所在的子目錄 include\ 來執行 fping.exe
-
+app = ENVPATH + '\\' + 'fping.exe'
 
 
 subprocess.run(['D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling\\fping.exe', '127.0.0.1', '-n', '2'])
