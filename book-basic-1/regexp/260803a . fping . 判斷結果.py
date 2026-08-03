408d21287974aa373818e5dabbc4ccf231fe619b
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-08-03 09:56:05 +0800

    Update 260803a . fping . 判斷結果.py

--- book-basic-1/regexp/260803a . fping . 判斷結果.py
+++ book-basic-1/regexp/260803a . fping . 判斷結果.py
@@ -1,19 +1,3 @@
-get_info = """
-Fast pinger version 3.00
-(c) Wouter Dhondt (http://www.kwakkelflap.com)
-
-Pinging 192.168.0.200 with 32 bytes of data every 1000 ms:
-
-Reply[1] from 192.168.0.200: bytes=32 time=22.2 ms TTL=100
-Reply[2] from 192.168.0.200: bytes=32 time=1.5 ms TTL=100
-Reply[3] from 192.168.0.200: bytes=32 time=1.0 ms TTL=100
-
-Ping statistics for 192.168.0.200:
-        Packets: Sent = 3, Received = 3, Lost = 0 (0% loss)
-Approximate round trip times in milli-seconds:
-        Minimum = 1.0 ms, Maximum = 22.2 ms, Average = 8.2 ms
-"""
-
 
 get_info = """
 Fast pinger version 3.00
@@ -29,6 +13,5 @@ Ping statistics for 192.168.0.200:
         Packets: Sent = 3, Received = 3, Lost = 0 (0% loss)
 Approximate round trip times in milli-seconds:
         Minimum = 1.0 ms, Maximum = 22.2 ms, Average = 8.2 ms
-CCC
 """
 print(get_info)
