8f6871e4605f3e9f5ccb47521b8abed90e246e4a
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-08-03 10:47:14 +0800

    Update 260803a . fping.py

--- book-basic-2/執行外部指令 . subprocess/260803a . fping.py
+++ book-basic-2/執行外部指令 . subprocess/260803a . fping.py
@@ -6,11 +6,30 @@ import os
 import subprocess
 from pathlib import Path
 
+subprocess.run(['D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling\\fping.exe', '127.0.0.1', '-n', '2'])
+# 輸出
+>>> subprocess.run(['D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling\\fping.exe', '127.0.0.1', '-n', '2'])
+
+Fast pinger version 3.00
+(c) Wouter Dhondt (http://www.kwakkelflap.com)
+
+Pinging 127.0.0.1 with 32 bytes of data every 1000 ms:
+
+Reply[1] from 127.0.0.1: bytes=32 time=0.1 ms TTL=128
+Reply[2] from 127.0.0.1: bytes=32 time=0.0 ms TTL=128
+
+Ping statistics for 127.0.0.1:
+        Packets: Sent = 2, Received = 2, Lost = 0 (0% loss)
+Approximate round trip times in milli-seconds:
+        Minimum = 0.0 ms, Maximum = 0.1 ms, Average = 0.1 ms
+CompletedProcess(args=['D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling\\fping.exe', '127.0.0.1', '-n', '2'], returncode=0)
+>>>
+
+# 建立 路徑變數 縮短指令長度.
 os.chdir('D:\\Dropbox\\14-Office-TryTryLu\\Python_01_RS232_RebootCycling\\')
 ENVPATH = Path.cwd()
 
-
-subprocess.run(['ping', '127.0.0.1'])
+subprocess.run(['fping.exe', '127.0.0.1', '-n', '2'])
 
 
 輸出:
