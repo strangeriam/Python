dde58eecbdf33130190a60d6f236281437897caa
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-08-03 09:54:39 +0800

    Update 260803a . fping . 判斷結果.py

--- book-basic-1/regexp/260803a . fping . 判斷結果.py
+++ book-basic-1/regexp/260803a . fping . 判斷結果.py
@@ -26,6 +26,9 @@ Reply[2] from 192.168.0.200: bytes=32 time=1.5 ms TTL=100
 Reply[3] from 192.168.0.200: bytes=32 time=1.0 ms TTL=100
 
 Ping statistics for 192.168.0.200:
+        Packets: Sent = 3, Received = 3, Lost = 0 (0% loss)
+Approximate round trip times in milli-seconds:
+        Minimum = 1.0 ms, Maximum = 22.2 ms, Average = 8.2 ms
 CCC
 """
 print(get_info)
