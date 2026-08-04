d97beb7dc3d13948232ff5ef56dd03aff890a4a3
Rlulu <64244191+strangeriam@users.noreply.github.com>
2026-08-04 15:40:45 +0800

    Update 260720 . def . 跨檔案.py

--- book-basic-1/def/260720 . def . 跨檔案.py
+++ book-basic-1/def/260720 . def . 跨檔案.py
@@ -1,7 +1,7 @@
-# 讀入 位在 include/apc.py 的 _f_apc_info.
+# 只使用 1 個 Function --> 讀入 位在 include/apc.py 的 _f_apc_info.
 # 要使用 _f_apc_info 的功能.
 
 import include.apc as _f_apc_info
 
-# 讀入 位在 include/apc.py 的 _f_apc_info & _f_apc_power.
-import include.apc as _f_apc_info, _f_apc_power
+# 使用 多 個 Functions --> 讀入 位在 include/apc.py 的 _f_apc_info & _f_apc_power.
+from include.apc import _f_apc_info, _f_apc_power
